ecbcb5b680c22389546247af954e0ff5b42de212
Chris Beer <cabeer@stanford.edu>
2014-04-11 11:57:00 -0700

    add blacklight bookmarks migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411172802) do
+ActiveRecord::Schema.define(version: 20140411185614) do
 
   create_table "bookmarks", force: true do |t|
-    t.integer  "user_id",     null: false
+    t.integer  "user_id",       null: false
     t.string   "user_type"
     t.string   "document_id"
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "document_type"
   end
+
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
