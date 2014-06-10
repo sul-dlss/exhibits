92bda4c4eaa6685cf88d70e51842fa39b1efddf7
Chris Beer <cabeer@stanford.edu>
2014-06-10 07:57:06 +0300

    add public/uploads as a linked directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :log_level, :info
 set :linked_files, %w{config/database.yml config/solr.yml config/harvestdor.yml config/initializers/secret_token.rb config/initializers/squash.rb public/.htaccess}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system config/environments}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads config/environments}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
