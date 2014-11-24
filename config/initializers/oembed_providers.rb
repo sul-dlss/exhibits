1db99c7e288be54bd1700b024221cd980009a29c
Chris Beer <cabeer@stanford.edu>
2014-11-24 09:02:56 -0800

    Register purl oembed provider

--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -1,3 +1,9 @@
 require 'oembed'
 
 OEmbed::Providers.register_all
+
+purl_provider = OEmbed::Provider.new("http://purl.stanford.edu/embed")
+purl_provider << "http://purl.stanford.edu/*"
+purl_provider << "https://purl.stanford.edu/*"
+purl_provider << "http://searchworks.stanford.edu/*"
+OEmbed::Providers.register(purl_provider)
