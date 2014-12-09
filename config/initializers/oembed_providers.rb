45cf7ec9aa3aa98606d8895e4d710f19b372296d
Chris Beer <cabeer@stanford.edu>
2014-12-09 10:37:48 -0800

    Hide the embedded title from purl resources

--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -2,7 +2,7 @@ require 'oembed'
 
 OEmbed::Providers.register_all
 
-purl_provider = OEmbed::Provider.new("http://purl.stanford.edu/embed")
+purl_provider = OEmbed::Provider.new("http://purl.stanford.edu/embed.{format}?&hide_title=true")
 purl_provider << "http://purl.stanford.edu/*"
 purl_provider << "https://purl.stanford.edu/*"
 purl_provider << "http://searchworks.stanford.edu/*"
