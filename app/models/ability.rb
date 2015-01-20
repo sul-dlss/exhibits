9b2c568ab7a592b0035851116213cb18dbeb0361
Chris Beer <cabeer@stanford.edu>
2015-01-20 15:03:54 -0800

    Fix Ability to work with anonymous users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include Spotlight::Ability
   def initialize(user)
     super
-    can :manage, PurlResource, exhibit_id: user.roles.pluck(:exhibit_id)
+
+    can :manage, PurlResource, exhibit_id: user.roles.pluck(:exhibit_id) if user
   end
 end
