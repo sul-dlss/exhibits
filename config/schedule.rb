a1ce8b23503c97ecfb81a21904227542e98ea38e
Chris Beer <cabeer@stanford.edu>
2015-02-03 08:09:49 -0800

    Disable whenever scheduled tasks; to limit load on the server, we either need to randomize the times or let the requests app manage the schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, File.join("log", "cron.log")
-
-every :day, :at => '2:30am', :roles => [:app] do
-  rake "spotlight:reindex"
-end
-
-every 1.day, :at => '5:00 am' do
-  rake "-s sitemap:refresh"
-end
+# 
+# every :day, :at => '2:30am', :roles => [:app] do
+#   rake "spotlight:reindex"
+# end
+# 
+# every 1.day, :at => '5:00 am' do
+#   rake "-s sitemap:refresh"
+# end
