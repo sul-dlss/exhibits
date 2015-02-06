5117a2aea6ac9f8056ce4a2bef238598f6eac738
Chris Beer <cabeer@stanford.edu>
2015-02-05 16:53:15 -0800

    Configure the carrierwave base path if we're running under a suburi

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |c| 
+  carrierwave_base_path = Settings.carrierwave_base_path if Settings.carrierwave_base_path
+  carrierwave_base_path ||= begin
+    File.read(File.join(Rails.root, "public", ".htaccess")).scan(/PassengerBaseURI (.*)$/).first.try(:first)
+  end
+
+  c.base_path = carrierwave_base_path
+end
