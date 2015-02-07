aa8acd4364b89272ea8c0dc5bc01c8cbc9998a75
Chris Beer <cabeer@stanford.edu>
2015-02-06 19:27:45 -0800

    Check if the .htaccess file exists before trying to read from it

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |c|
   carrierwave_base_path = Settings.carrierwave_base_path if Settings.carrierwave_base_path
   carrierwave_base_path ||= begin
     File.read(File.join(Rails.root, "public", ".htaccess")).scan(/PassengerBaseURI (.*)$/).first.try(:first)
-  end
+  end if File.exists? File.join(Rails.root, "public", ".htaccess")
 
   c.base_path = carrierwave_base_path
 end
