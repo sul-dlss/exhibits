c27e663e2f99dbf2f382de9e2fed68b32129ebc5
Chris Beer <cabeer@stanford.edu>
2015-03-03 08:23:50 -0800

    Copy the uploaded item title field into the relevant display and search title fields

--- config/initializers/spotlight_initializer.rb
+++ config/initializers/spotlight_initializer.rb
@@ -1,1 +1,2 @@
 Spotlight::Engine.config.new_resource_partials += ['purl_resources/form']
+Spotlight::Engine.config.upload_title_field = OpenStruct.new(field_name: 'title', solr_field: ['title_full_display', 'title_display', 'title_245_search'])
