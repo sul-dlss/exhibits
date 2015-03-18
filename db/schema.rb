dc7f6ff31fb8c4121b2afd07bbb2f08f8f522e27
Chris Beer <cabeer@stanford.edu>
2015-03-18 12:02:49 -0700

    Update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309222823) do
+ActiveRecord::Schema.define(version: 20150318185717) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 20150309222823) do
     t.text     "configuration"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "field_type"
   end
 
   create_table "spotlight_exhibits", force: :cascade do |t|
