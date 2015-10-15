0eeb8700c426f62cb39b7067b6a46fa330251830
Chris Beer <cabeer@stanford.edu>
2015-10-15 08:23:15 -0700

    Expose MODS xml as a blacklight document export format; fixes #30

---
+++ app/models/concerns/mods_document.rb
@@ -0,0 +1,14 @@
+module ModsDocument
+
+  def self.extended(document)
+    ModsDocument.register_export_formats(document)
+  end
+
+  def self.register_export_formats(document)
+    document.will_export_as(:mods)
+  end
+
+  def export_as_mods
+    self[:modsxml]
+  end
+end
