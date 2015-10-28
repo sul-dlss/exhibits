74318f1295585c8609cca5a258b0cfe7f5c46d84
Naomi Dushay <ndushay@stanford.edu>
2015-10-28 16:52:14 -0700

    better tests for box

--- spec/unit/spotlight/dor/indexer_spec.rb
+++ spec/unit/spotlight/dor/indexer_spec.rb
@@ -120,71 +120,60 @@ describe Spotlight::Dor::Indexer do
   end # add_series
 
   describe "#add_box" do
-    parsable_exemplars_based_on_actual_data = [
+    # example string as key, expected box name as value
+    {
       # feigenbaum
-      'Call Number: SC0340, Accession 2005-101, Box : 42, Folder: 1',
-      'Call Number: SC0340, Accession 2005-101, Box: 42, Folder: 3',
-      'Call Number: SC0340, Accession: 1986-052, Box 42 Folder 38',
-      'Call Number: SC0340, Accession: 2005-101, Box : 42, Folder: 20',
-      'Call Number: SC0340, Accession: 1986-052, Box: 42, Folder: 1',
-      'SC0340, 1986-052, Box 42',
-      'SC0340, Accession 1986-052, Box 42',
-      # shpc
-      'Series Biographical Photographs | Box 42 | Folder Abbot, Nathan',
-      'Series General Photographs | Box 42 | Folder Administration building--Outer Quad',
+      'Call Number: SC0340, Accession 2005-101, Box : 1, Folder: 1': '1',
+      'Call Number: SC0340, Accession 2005-101, Box: 39, Folder: 9': '39',
+      'Call Number: SC0340, Accession: 1986-052, Box 3 Folder 38': '3',
+      'Call Number: SC0340, Accession: 2005-101, Box : 50, Folder: 31': '50',
+      'Call Number: SC0340, Accession: 1986-052, Box: 5, Folder: 1': '5',
+      'SC0340, 1986-052, Box 18': '18',
+      'SC0340, Accession 2005-101, Box 18': '18',
+      'Call Number: SC0340, Accession 2005-101, Box: 42A, Folder: 24': '42A',
+      'Call Number: SC0340, Accession: 1986-052, Box: 42A, Folder: 59': '42A',
+      'Call Number: SC0340, Accession 2005-101': nil,
+      'Call Number: SC0340, Accession: 1986-052': nil,
+      'SC0340': nil,
+      'SC0340, Accession 1986-052': nil,
+      'Stanford University. Libraries. Department of Special Collections and University Archives': nil,
+      # shpc (actually in <relatedItem><location><physicalLocation>)
+      'Series Biographical Photographs | Box 42 | Folder Abbot, Nathan': '42',
+      'Series General Photographs | Box 42 | Folder Administration building--Outer Quad': '42',
       # menuez
-      'MSS Photo 451, Series 1, Box 42, Folder 11, Sleeve 32-11-2, Frame B32-F11-S2-6',
-      'Series 1, Box 42, Folder 8',
+      'MSS Photo 451, Series 1, Box 32, Folder 11, Sleeve 32-11-2, Frame B32-F11-S2-6': '32',
+      'Series 1, Box 10, Folder 8': '10',
       # fuller
-      'Collection: M1090 , Series: 4 , Box: 42 , Folder: 10',
+      'Collection: M1090 , Series: 1 , Box: 5 , Folder: 42': '5',
       # hummel (actually in <relatedItem><location><physicalLocation>)
-      'Box 42 | Folder 3',
-      'Flat-box 42 | Volume 1'
-    ]
-    parsable_exemplars_based_on_actual_data.each do |example|
-      it "parses box number from physLoc '#{example}'" do
-        ng_mods = Nokogiri::XML("#{mods_loc_phys_loc_start}#{example}#{mods_loc_phys_loc_end}")
-        allow(r).to receive(:mods).and_return(ng_mods)
-        subject.send(:add_box, sdb, solr_doc)
-        expect(solr_doc['box_ssi']).to eq '42'
-      end
-      it "parses box number from relItem physLoc '#{example}'" do
-        ng_mods = Nokogiri::XML("#{mods_rel_item_loc_phys_loc_start}#{example}#{mods_rel_item_loc_phys_loc_end}")
-        allow(r).to receive(:mods).and_return(ng_mods)
-        subject.send(:add_box, sdb, solr_doc)
-        expect(solr_doc['box_ssi']).to eq '42'
-      end
-    end
-    boxes_with_letters = [
-      # feigenbaum
-      'Call Number: SC0340, Accession 2005-101, Box: 42A, Folder: 24',
-      'Call Number: SC0340, Accession: 1986-052, Box: 42A, Folder: 59'
-    ]
-    boxes_with_letters.each do |example|
-      it "parses box number from '#{example}'" do
-        ng_mods = Nokogiri::XML("#{mods_loc_phys_loc_start}#{example}#{mods_loc_phys_loc_end}")
-        allow(r).to receive(:mods).and_return(ng_mods)
-        subject.send(:add_box, sdb, solr_doc)
-        expect(solr_doc['box_ssi']).to eq '42A'
-      end
-    end
-
-    unparsable_exemplars_based_on_actual_data = [
-      # feigenbaum
-      'Call Number: SC0340, Accession 2005-101',
-      'Call Number: SC0340, Accession: 1986-052',
-      'SC0340',
-      'SC0340, Accession 1986-052',
-      'Stanford University. Libraries. Department of Special Collections and University Archives'
-    ]
-    unparsable_exemplars_based_on_actual_data.each do |example|
-      it "does not parse box number from '#{example}'" do
-        ng_mods = Nokogiri::XML("#{mods_loc_phys_loc_start}#{example}#{mods_loc_phys_loc_end}")
-        allow(r).to receive(:mods).and_return(ng_mods)
-        subject.send(:add_box, sdb, solr_doc)
-        expect(solr_doc['box_ssi']).to be_falsey
-      end
-    end
+      'Box 42 | Folder 3': '42',
+      'Flat-box 228 | Volume 1': '228'
+    }.each do |example, expected|
+      describe "for example '#{example}'" do
+        let(:example) { example }
+        context 'in /location/physicalLocation' do
+          before do
+            i = Harvestdor::Indexer.new
+            i.logger.level = Logger::WARN
+            allow(r).to receive(:indexer).and_return(i)
+            allow(r).to receive(:mods).and_return(mods_loc_phys_loc)
+            subject.send(:add_box, sdb, solr_doc)
+          end
+          it "has the expected box name '#{expected}'" do
+            expect(solr_doc['box_ssi']).to eq expected
+          end
+        end
+        context 'in /relatedItem/location/physicalLocation' do
+          before do
+            allow(r).to receive(:mods).and_return(mods_rel_item_loc_phys_loc)
+            subject.send(:add_box, sdb, solr_doc)
+          end
+          it "has the expected box name '#{expected}'" do
+            expect(solr_doc['box_ssi']).to eq expected
+          end
+        end
+      end # for example
+    end # each
   end # add_box
 
   describe '#add_folder' do
