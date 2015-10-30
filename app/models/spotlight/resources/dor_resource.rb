ddb25f6718006fbfccf3b59a030f1c87bcc44605
Chris Beer <cabeer@stanford.edu>
2015-10-30 10:53:13 -0700

    Update DorResources to use lazy-enumerated collection members

--- app/models/spotlight/resources/dor_resource.rb
+++ app/models/spotlight/resources/dor_resource.rb
@@ -1,17 +1,47 @@
 module Spotlight::Resources
   class DorResource < Spotlight::Resource
+    ##
+    # Generate solr documents for the DOR resources identified by this object
+    #
+    # @return [Enumerator] an enumerator of solr document hashes for indexing
     def to_solr
       return to_enum :to_solr unless block_given?
 
       base_doc = super
 
-      [resource, (resource.items if resource.collection?)].flatten.compact.each do |res|
-        yield base_doc.merge(Spotlight::Dor::Resources.indexer.solr_document(res))
+      indexable_resources.each do |res|
+        yield base_doc.merge(to_solr_document(res))
       end
     end
 
     def resource
       @resource ||= Spotlight::Dor::Resources.indexer.resource doc_id
+    end
+
+    private
+
+    ##
+    # Enumerate the resource, and any collection members, that should be indexed
+    # into this exhibit
+    #
+    # @return [Enumerator] an enumerator of resources to index
+    def indexable_resources
+      return to_enum(:indexable_resources) unless block_given?
+
+      yield resource
+
+      resource.items.each do |r|
+        yield r
+      end
+    end
+
+    ##
+    # Generate the solr document hash for a given resource by applying the current
+    # indexer steps.
+    #
+    # @return [Hash]
+    def to_solr_document(resource)
+      Spotlight::Dor::Resources.indexer.solr_document(resource)
     end
   end
 end
