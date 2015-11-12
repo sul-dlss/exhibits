558d770e63db0848d554724a0497a432e73aa31a
Chris Beer <cabeer@stanford.edu>
2015-11-11 17:12:55 -0800

    Update PurlResource to prevent creating duplicate resource records

--- app/models/purl_resource.rb
+++ app/models/purl_resource.rb
@@ -4,20 +4,24 @@ class PurlResource
 
   attr_accessor :data, :exhibit
 
-  def self.purls(exhibit)
-    Spotlight::Resources::Purl.where(exhibit: exhibit).pluck(:url).map do |x|
+  def self.druids(exhibit)
+    resources(exhibit).pluck(:url).map do |x|
       x.match(%r{^https?://purl.stanford.edu/([^/\.]+)})[1]
     end.uniq
+  end
+
+  def self.resources(exhibit)
+    Spotlight::Resources::Purl.where(exhibit: exhibit)
   end
 
   delegate :id, to: :exhibit, prefix: true
 
   def save
     data.split("\n").map(&:strip).reject(&:blank?).each do |line|
-      if line =~ /^\w{2}\d{3}\w{2}\d{4}$/
-        r = Spotlight::Resources::Purl.new exhibit: exhibit, url: "https://purl.stanford.edu/#{line}"
-        r.save!
-      end
+      next unless line =~ /^\w{2}\d{3}\w{2}\d{4}$/
+      exhibit.resources.find_or_create_by(type: 'Spotlight::Resources::Purl',
+                                          exhibit: exhibit,
+                                          url: "https://purl.stanford.edu/#{line}")
     end
   end
 end
