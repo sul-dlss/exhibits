893ee427d3126566be4d4fde830d4963d341b02f
Chris Beer <cabeer@stanford.edu>
2015-11-16 16:01:21 -0800

    Include the base path when generating the sitemap for an exhibit

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,6 @@
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = Settings.host
+SitemapGenerator::Sitemap.default_host = "#{Settings.host}#{Settings.base_path}"
 SitemapGenerator::Interpreter.send :include, Rails.application.routes.url_helpers
 SitemapGenerator::Interpreter.send :include, Spotlight::Engine.routes.url_helpers
 SitemapGenerator::Sitemap.create do
