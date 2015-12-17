11130db4e0e870024ea0cac5f7e325abbbf84c41
Chris Beer <cabeer@stanford.edu>
2015-12-16 18:12:03 -0800

    Start a bunch of delayed job workers in production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,5 @@ server "#{fetch(:deploy_host)}", user: fetch(:user), roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
+
+set :delayed_job_workers, 8
