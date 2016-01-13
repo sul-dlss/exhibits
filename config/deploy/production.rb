430343f80aba44e6f24e65a52e07ed0e34222b66
Peter Mangiafico <peter@mangiafico.org>
2016-01-12 21:30:27 -0800

    update exhibits cap prod deployment to automatically deploy to both -a and -b servers automatically

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,12 @@
-set :deploy_host, ENV['CAPISTRANO_PRODUCTION_DEPLOY_HOST'] || ask(:deploy_host, '')
+set :deploy_host, ENV['CAPISTRANO_PRODUCTION_DEPLOY_HOST'] || ask('deploy_host prefix (leave off stanford.edu)', '')
 set :user, ENV['CAPISTRANO_PRODUCTION_USER'] || ask(:user, '')
+set :bundle_without, %w(deployment development test).join(' ')
 
-server "#{fetch(:deploy_host)}", user: fetch(:user), roles: %w(web db app)
+%w(a b).each do |suffix|
+  server "#{fetch(:deploy_host)}-#{suffix}.stanford.edu",
+         user: fetch(:user),
+         roles: %w(web db app)
+end
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
