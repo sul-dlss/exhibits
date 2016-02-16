88237e890b372753e8a172bf423c3f2d7e0ce71d
Chris Beer <cabeer@stanford.edu>
2016-02-16 11:07:53 -0800

    Add deployment hosts and users

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,5 @@
-set :deploy_host, ENV['CAPISTRANO_PRODUCTION_DEPLOY_HOST'] || ask(:deploy_host, '')
-set :user, ENV['CAPISTRANO_PRODUCTION_USER'] || ask(:user, '')
-
-server "#{fetch(:deploy_host)}", user: fetch(:user), roles: %w(web db app)
+server 'exhibits-prod-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
+server 'exhibits-prod-b.stanford.edu', user: 'exhibits', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
