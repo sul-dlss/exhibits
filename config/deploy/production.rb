642a9c9a5527ac4d5b0efea407fc88ca6dc2e2b2
Chris Beer <chris@cbeer.info>
2016-07-11 14:46:22 -0700

    Deploy the application to the new worker VM

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,10 @@
 server 'exhibits-prod-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
-server 'exhibits-prod-b.stanford.edu', user: 'exhibits', roles: %w(web db app)
+server 'exhibits-prod-b.stanford.edu', user: 'exhibits', roles: %w(web app)
+server 'exhibits-worker-prod-a.stanford.edu', user: 'exhibits', roles: %w(app background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
 
 set :delayed_job_workers, 8
+set :delayed_job_roles, [:background]
+set :delayed_job_monitor, true
