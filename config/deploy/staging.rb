f6c2b566b5105828245c3223780c0c40e2315693
Chris Beer <chris@cbeer.info>
2016-07-12 14:22:00 -0700

    Deploy the application to the new staging environment (#240)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,10 @@
-
-server 'exhibits-stage.stanford.edu', user: 'exhibits', roles: %w(web db app)
+server 'exhibits-stage-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
+server 'exhibits-stage-b.stanford.edu', user: 'exhibits', roles: %w(web app)
+server 'exhibits-worker-stage-a.stanford.edu', user: 'exhibits', roles: %w(app background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
 
 set :delayed_job_workers, 4
+set :delayed_job_roles, [:background]
+set :delayed_job_monitor, true
