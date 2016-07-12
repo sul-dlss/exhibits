be70f19f6d8e019d045a9a2521a6df2ecfc98afd
Chris Beer <chris@cbeer.info>
2016-07-12 07:20:11 -0700

    Add the collection title as a facet and display field (#237)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # :nodoc:
 module ApplicationHelper
+  # Collection titles are indexed as a compound druid + title; we need to
+  # unmangle it for display.
+  def collection_title(value, separator: '-|-')
+    value.split(separator).last
+  end
+
+  def document_collection_title(value:, **)
+    Array(value).map { |v| collection_title(v) }.to_sentence
+  end
 end
