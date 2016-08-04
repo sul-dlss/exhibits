4ae8c3858ae6e0b938cc795f0315fd63b2c2dda9
Chris Beer <chris@cbeer.info>
2016-08-04 11:35:08 -0700

    Fix parameterized heatmap document path; fixes #306 (#308)

---
+++ spec/helpers/blacklight/maps_helper_override_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Blacklight::MapsHelperOverride, type: :helper do
+  let(:exhibit) { FactoryGirl.create(:exhibit) }
+
+  before do
+    helper.extend(Module.new do
+      def current_exhibit; end
+
+      def blacklight_config; end
+    end)
+    allow(helper).to receive(:current_exhibit).and_return(exhibit)
+    allow(helper).to receive(:blacklight_config).and_return(exhibit.blacklight_config)
+  end
+
+  describe '#document_path' do
+    it 'is a parameterized path for documents' do
+      expect(helper.send(:document_path)).to eq "/#{exhibit.slug}/catalog/{id}"
+    end
+  end
+end
