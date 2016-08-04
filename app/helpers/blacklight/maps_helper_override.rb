06d59efe3ec0ba1177aab88e63a6e8c870f78e97
Chris Beer <chris@cbeer.info>
2016-08-04 11:10:09 -0700

    Fix parameterized heatmap document path; fixes #306

--- app/helpers/blacklight/maps_helper_override.rb
+++ app/helpers/blacklight/maps_helper_override.rb
@@ -3,8 +3,13 @@ module Blacklight
   module MapsHelperOverride
     private
 
+    ##
+    # Build a parameterized path for the solr documents
+    # and avoid Rails URL escaping by using a temporary token, and then substituting the actual value.
     def document_path
-      exhibit_solr_document_path(exhibit: current_exhibit, id: :id)
+      token = '__:id:__'
+      original_path = spotlight.exhibit_solr_document_path(exhibit_id: current_exhibit, id: token)
+      original_path.sub(token, "{#{blacklight_config.document_model.unique_key}}")
     end
   end
 end
