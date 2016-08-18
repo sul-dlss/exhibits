7d35d866fb30cccffa509fe84a9201e0de171d67
Chris Beer <chris@cbeer.info>
2016-08-18 09:19:56 -0700

    Improve redirect after login to include a fallback location; fixes #335 (#336)

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -4,12 +4,31 @@ describe LoginController do
   let(:user) { create(:curator) }
   before do
     allow_any_instance_of(ApplicationController).to receive_messages(current_user: user)
-    request.env['HTTP_REFERER'] = 'http://example.com'
   end
 
-  it 'accepts the user invitation on login and redirects to the referrer' do
-    expect(user).to receive(:accept_invitation!)
-    get :login, referrer: '/home'
-    expect(response).to redirect_to '/home'
+  context 'with an explicit referrer parameter' do
+    it 'accepts the user invitation on login and redirects to the referrer' do
+      expect(user).to receive(:accept_invitation!)
+      get :login, referrer: '/home'
+      expect(response).to redirect_to '/home'
+    end
+  end
+
+  context 'with a referrer header' do
+    before do
+      request.env['HTTP_REFERER'] = 'http://example.com'
+    end
+
+    it 'redirects to the referrer' do
+      get :login
+      expect(response).to redirect_to 'http://example.com'
+    end
+  end
+
+  context 'without any referrer information' do
+    it 'redirects to the home page' do
+      get :login
+      expect(response).to redirect_to '/'
+    end
   end
 end
