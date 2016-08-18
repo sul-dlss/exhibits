0abb92e025a39e601cc55c7c7766df0cc6c47434
Chris Beer <chris@cbeer.info>
2016-08-17 21:28:24 -0700

    Update dependencies

---
+++ db/migrate/20160818140248_add_index_status_to_solr_document_sidecar.spotlight.rb
@@ -0,0 +1,6 @@
+# This migration comes from spotlight (originally 20160816165432)
+class AddIndexStatusToSolrDocumentSidecar < ActiveRecord::Migration
+  def change
+    add_column :spotlight_solr_document_sidecars, :index_status, :binary
+  end
+end
