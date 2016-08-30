653649b5b9a165015d7b807844ab3bff1d4a83dc
Chris Beer <chris@cbeer.info>
2016-08-30 15:37:08 -0700

    Remove any trailing slash from the solr connection string

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -4,6 +4,6 @@ OkComputer.mount_at = false
 OkComputer::Registry.register "version", OkComputer::AppVersionCheck.new
 OkComputer::Registry.register "cache", OkComputer::CacheCheck.new
 OkComputer::Registry.register "delayed_jobs", OkComputer::DelayedJobBackedUpCheck.new(10, 50)
-OkComputer::Registry.register "solr", OkComputer::SolrCheck.new(Blacklight.default_index.connection.uri.to_s)
+OkComputer::Registry.register "solr", OkComputer::SolrCheck.new(Blacklight.default_index.connection.uri.to_s.sub(/\/$/, ''))
 
 OkComputer.make_optional %w(version cache delayed_jobs)
