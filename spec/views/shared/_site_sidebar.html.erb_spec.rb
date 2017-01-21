bddddb58417f08d007099666989f911f7ba17906
Chris Beer <chris@cbeer.info>
2017-01-20 16:16:52 -0800

    Update link text on Exhibits landing page
    Fixes #419

--- spec/views/shared/_site_sidebar.html.erb_spec.rb
+++ spec/views/shared/_site_sidebar.html.erb_spec.rb
@@ -7,7 +7,8 @@ describe 'shared/_site_sidebar', type: :view do
       allow(view).to receive(:can?).with(:manage, Spotlight::Site.instance) { false }
       render
     end
-    it { is_expected.not_to have_link('Request a new exhibit') }
+    it { is_expected.to have_link('Request an exhibit') }
+    it { is_expected.not_to have_link('Create a new exhibit') }
   end
 
   context 'with an admin user' do
@@ -17,6 +18,6 @@ describe 'shared/_site_sidebar', type: :view do
       allow(view).to receive(:can?).with(:manage, Spotlight::Site.instance) { true }
       render
     end
-    it { is_expected.to have_link('Request a new exhibit') }
+    it { is_expected.to have_link('Create a new exhibit') }
   end
 end
