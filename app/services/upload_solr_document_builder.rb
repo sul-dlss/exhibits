0da5e5242c638bdb0da022044ef7bc9569ff556e
Chris Beer <chris@cbeer.info>
2017-02-08 08:11:28 -0800

    Add custom UploadSolrDocumentBuilder that adds square thumbnail urls to the index

---
+++ app/services/upload_solr_document_builder.rb
@@ -0,0 +1,8 @@
+# Custom upload solr document builder that adds square thumbnail urls
+class UploadSolrDocumentBuilder < Spotlight::UploadSolrDocumentBuilder
+  def add_file_versions(solr_hash)
+    super(solr_hash)
+
+    solr_hash[:thumbnail_square_url_ssm] = riiif.image_path(resource.upload_id, region: 'square', size: '100,100')
+  end
+end
