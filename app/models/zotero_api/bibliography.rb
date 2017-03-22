0ac768df3dc1b6dc69ec8cb21a711b0b256c8c66
Jack Reed <phillipjreed@gmail.com>
2017-03-22 16:10:58 -0400

    nix custom find method on Bibliography

--- app/models/zotero_api/bibliography.rb
+++ app/models/zotero_api/bibliography.rb
@@ -6,13 +6,6 @@ module ZoteroApi
       self.class.new(sort_by(&:to_author_date))
     end
 
-    # @param [String] `key` the bibliography item's `key` value
-    # @return [BibliographyItem] the matching item, or `nil`
-    def find(key)
-      each { |item| return item if item['key'] == key }
-      nil
-    end
-
     # @return [String] HTML for the entire bibliography, sorted by Author-Date
     def render
       "<ul>#{sort_by_author_date.collect(&:to_html).join}</ul>"
