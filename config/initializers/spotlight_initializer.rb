d8e36dc2e624a21a168d49f0c1810280396a5a98
Jessie Keck <jessie.keck@gmail.com>
2017-03-30 09:11:02 -0700

    Add title to title_sort field for uploaded resources.

--- config/initializers/spotlight_initializer.rb
+++ config/initializers/spotlight_initializer.rb
@@ -1,4 +1,4 @@
-Spotlight::Engine.config.upload_title_field = OpenStruct.new(field_name: 'title', solr_field: %w(title_full_display title_display title_245_search))
+Spotlight::Engine.config.upload_title_field = OpenStruct.new(field_name: 'title', solr_field: %w(title_full_display title_display title_245_search title_sort))
 Spotlight::Engine.config.default_contact_email = Settings.default_contact_email
 Spotlight::Engine.config.external_resources_partials += ['dor_harvester/form']
 
