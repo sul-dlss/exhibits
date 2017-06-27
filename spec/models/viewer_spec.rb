f2801fdfc0a9fd72bfb09361d6d94133ed2a9262
Scott Bailey <csb5t@virginia.edu>
2017-06-27 10:56:18 -0700

    Rename file for accuracy; be railsy about partial paths to provide for future possible viewers; chance variable type to indicate scope

--- spec/models/viewer_spec.rb
+++ spec/models/viewer_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 describe Viewer do
+  let(:viewer) { FactoryGirl.create(:viewer) }
+
   it 'belongs to an exhibit' do
     expect(described_class.reflect_on_association(:exhibit).macro).to eq :belongs_to
+  end
+
+  it 'has the correct partial path based on viewer_type' do
+    viewer.viewer_type = 'sul-embed'
+    expect(viewer.to_partial_path).to eq 'oembed_default'
+    viewer.viewer_type = 'mirador'
+    expect(viewer.to_partial_path).to eq '../viewers/mirador'
   end
 end
