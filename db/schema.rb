6a0a074d3f63204046ace22303ad13b4fbe87333
Justin Coyne <jcoyne@justincoyne.com>
2017-08-21 12:03:18 -0500

    Migrate index_status to LONGBLOB. Fixes #533

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515204158) do
+ActiveRecord::Schema.define(version: 20170821165811) do
 
   create_table "bibliography_services", force: :cascade do |t|
     t.string   "header"
@@ -330,7 +330,7 @@ ActiveRecord::Schema.define(version: 20170515204158) do
     t.string   "document_type"
     t.integer  "resource_id"
     t.string   "resource_type"
-    t.binary   "index_status"
+    t.binary   "index_status",  limit: 10485760
     t.index ["document_type", "document_id"], name: "spotlight_solr_document_sidecars_solr_document"
     t.index ["exhibit_id", "document_type", "document_id"], name: "spotlight_solr_document_sidecars_exhibit_document"
     t.index ["exhibit_id"], name: "index_spotlight_solr_document_sidecars_on_exhibit_id"
