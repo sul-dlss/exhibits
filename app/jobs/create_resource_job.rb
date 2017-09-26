df652cda8000aee468b5ba0c02d90eb62ea74212
Jack Reed <phillipjreed@gmail.com>
2017-09-26 16:58:51 -0400

    remove actual index job b/c it does not work (but it is the right approach

--- app/jobs/create_resource_job.rb
+++ app/jobs/create_resource_job.rb
@@ -4,13 +4,7 @@
 class CreateResourceJob < ActiveJob::Base
   queue_as :default
 
-  def perform(id, exhibit, json, metadata = {})
-    resource = Spotlight::Resource.find_or_initialize_by(url: id, exhibit_id: exhibit.id)
-    resource.attributes = { json: json, metadata: metadata }
-    unless resource.save
-      logger.warn "Unable to save resource #{id} because: #{resource.errors.full_messages}"
-      return
-    end
-    resource.reindex
+  def perform(id, exhibit, data)
+    # TODO: Implement with https://github.com/projectblacklight/spotlight/wiki/Resource-Scenarios#on-demand-resource-indexing
   end
 end
