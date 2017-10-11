3b6ded7002247d0d71508d2b1b95361955a2af39
Darren Hardy <drh@stanford.edu>
2017-10-11 13:09:55 -0700

    do not save search in history if JSON API

---
+++ spec/features/search_histories_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Search histories', type: :feature do
+  let(:exhibit) { FactoryGirl.create(:exhibit) }
+
+  before do
+    allow(Search).to receive(:create).and_call_original
+
+    # create a search in the history
+    visit spotlight.search_exhibit_catalog_path(exhibit_id: exhibit.slug, q: 'article')
+
+    # do a JSON or non-JSON query to see whether it's also added to the history
+    visit spotlight.search_exhibit_catalog_path(exhibit_id: exhibit.slug,
+                                                q: 'book',
+                                                format: format)
+  end
+
+  context 'when format is JSON' do
+    let(:format) { 'json' }
+
+    scenario 'second query is not added to search history' do
+      expect(Search).to have_received(:create).once
+    end
+  end
+
+  context 'when format is non-JSON' do
+    let(:format) { 'html' }
+
+    scenario 'second query is added to search history' do
+      expect(Search).to have_received(:create).twice
+    end
+  end
+end
