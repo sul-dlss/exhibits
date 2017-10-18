dd2ab237fcde8385b1f756fae932f7ba2667e92a
Darren Hardy <drh@stanford.edu>
2017-10-18 12:47:14 -0700

    add Annotation data model

---
+++ app/models/concerns/annotation_concern.rb
@@ -0,0 +1,24 @@
+##
+# A concern to be mixed into SolrDocument for accessing a document's
+# annotation
+module AnnotationConcern
+  def annotation?
+    first('format_main_ssim') == 'Annotation'
+  end
+
+  # @return [Annotation]
+  def annotation
+    return unless annotation?
+    Annotation.new(
+      id,
+      first('annotation_tesim'),
+      Annotation::Target.new(
+        first('xywh_ssim'),
+        first('canvas_ssim'),
+        first('related_document_id_ssim')
+      ),
+      language: first('language'),
+      motivation: first('motivation_ssim')
+    )
+  end
+end
