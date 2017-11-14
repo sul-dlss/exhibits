87f175336659a0e9117781dc2fb285dcb3232ef4
Chris Beer <cabeer@stanford.edu>
2017-11-14 13:58:29 -0800

    Refactor bibtex traject mapping to use more generalized macros

--- lib/traject/bibtex_config.rb
+++ lib/traject/bibtex_config.rb
@@ -9,6 +9,14 @@ require 'active_support/core_ext/object/blank'
 extend Macros::BibTeX
 extend Macros::General
 
+BIBTEX_ZOTERO_MAPPING = {
+  phdthesis: 'Thesis',
+  incollection: 'Book section',
+  article: 'Journal article',
+  book: 'Book',
+  misc: 'Document'
+}.freeze
+
 settings do
   provide 'reader_class_name', 'BibTeXReader'
   provide 'processing_thread_pool', ::Settings.traject.processing_thread_pool || 1
@@ -20,15 +28,21 @@ each_record do |record, context|
   context.skip!("Skipping #{record.key} no keywords") unless record.respond_to?(:keywords)
 end
 
-to_field 'id', extract_bibtex_id
-to_field 'bibtex_key_ss', extract_bibtex_key
-to_field 'ref_type_ssm', extract_bibtex_field(:ref_type)
+to_field 'id', extract_bibtex(:key) do |_record, accumulator, _context|
+  accumulator.map! { |v| v.gsub(%r{http://zotero.org/groups/\d*/items/}, '') }
+end
+to_field 'bibtex_key_ss', extract_bibtex(:key)
+to_field 'ref_type_ssm', extract_bibtex(:type) do |_record, accumulator, _context|
+  accumulator.map! { |v| BIBTEX_ZOTERO_MAPPING[v] }.compact!
+end
+
 to_fields %w(title_display title_full_display title_uniform_search title_sort), extract_bibtex_field(:title)
 to_fields %w(author_person_full_display author_sort), extract_bibtex_field(:author)
-to_fields %w(pub_year_isi pub_year_w_approx_isi), extract_bibtex_field(:year)
+to_fields %w(pub_year_isi pub_year_w_approx_isi), extract_bibtex(:year)
 to_field 'editor_ssim', extract_bibtex_field(:editor)
 to_field 'book_title_ssim', extract_bibtex_field(:booktitle)
-to_field 'pub_display', extract_bibtex_publication
+to_field 'pub_display', extract_bibtex_field(:journal)
+to_field 'pub_display', extract_bibtex_field(:publisher)
 to_field 'location_ssi', extract_bibtex_field(:address)
 to_field 'university_ssim', extract_bibtex_field(:school)
 to_field 'edition_ssm', extract_bibtex_field(:edition)
@@ -40,6 +54,9 @@ to_field 'pages_ssm', extract_bibtex_field(:pages)
 to_field 'doi_ssim', extract_bibtex_field(:doi)
 to_field 'general_notes_ssim', extract_bibtex_field(:annote, split: '|', trim: true)
 to_field 'format_main_ssim', literal('Reference')
-to_field 'bibtex_ts', extract_bibtex_raw
+to_field 'bibtex_ts', extract_bibtex(:to_s)
 to_field 'formatted_bibliography_ts', extract_bibtex_formatted_bibliography
-to_field 'related_document_id_ssim', extract_bibtex_related_document_ids
+to_field 'related_document_id_ssim', extract_bibtex_field(:keywords,
+                                                          split: ',',
+                                                          trim: true,
+                                                          match: Exhibits::Application.config.druid_regex)
