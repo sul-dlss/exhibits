c386ab64a32f9d06ba379320349283ffb625ba32
Jack Reed <phillipjreed@gmail.com>
2017-11-15 07:27:32 -0800

    only show view all metadata when mods-xml is present

---
+++ spec/views/catalog/_metadata_button_default.html.erb_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'catalog/_metadata_button_default', type: :view do
+  let(:document) { SolrDocument.new(modsxml: 'stuff', id: 'abc') }
+  let(:current_exhibit) { create(:exhibit) }
+
+  before do
+    assign(:document, document)
+  end
+
+  context 'when modsxml is available' do
+    it do
+      expect(view).to receive_messages(current_exhibit: current_exhibit)
+      render
+      expect(rendered).to have_css 'a', text: 'View all metadata »'
+    end
+  end
+
+  context 'when modsxml is missing' do
+    let(:document) { SolrDocument.new }
+
+    it do
+      render
+      expect(rendered).not_to have_css 'a', text: 'View all metadata »'
+    end
+  end
+end
