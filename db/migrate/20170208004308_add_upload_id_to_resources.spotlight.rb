ab08f68da3cf9e1d55b8c6a2f7ef1f54b552883e
Darren Hardy <drh@stanford.edu>
2017-11-27 13:55:24 -0800

    Use explicit [5.0] for all db migrations

--- db/migrate/20170208004308_add_upload_id_to_resources.spotlight.rb
+++ db/migrate/20170208004308_add_upload_id_to_resources.spotlight.rb
@@ -1,5 +1,5 @@
 # This migration comes from spotlight (originally 20160805143841)
-class AddUploadIdToResources < ActiveRecord::Migration
+class AddUploadIdToResources < ActiveRecord::Migration[5.0]
   def change
     add_column :spotlight_resources, :upload_id, :integer
     add_index :spotlight_resources, :upload_id
