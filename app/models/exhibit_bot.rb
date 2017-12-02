7562e34063bba6128b542bebfdf6a1a7985a2dd9
Jessie Keck <jessie.keck@gmail.com>
2017-12-02 11:57:32 -0800

    Add an ExhibitBot wrapper class around the Slack::Web::Client.

---
+++ app/models/exhibit_bot.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+##
+# A wrapper class around the Slack Web Client.
+# All public methods are delegated through the class itself so things like
+# ExhibitBot.message(text: 'My Message Content') will simply work anywhere.
+class ExhibitBot
+  class << self
+    delegate :message, to: :new
+  end
+
+  def message(channel: default_channel, as_user: true, text:)
+    client.chat_postMessage(channel: channel, as_user: as_user, text: text)
+  end
+
+  private
+
+  def default_channel
+    Settings.slack_notifications.default_channel
+  end
+
+  def client
+    @client ||= if FeatureFlags.new.slack_notifications?
+                  Slack::Web::Client.new
+                else
+                  NullClient.new
+                end
+  end
+
+  # rubocop: disable Naming/MethodName
+  # NullObject pattern returned when we're not configured to send slack notifications
+  class NullClient
+    # noop
+    def chat_postMessage(*args); end
+  end
+  # rubocop: enable Naming/MethodName
+end
---
+++ spec/models/exhibit_bot_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ExhibitBot do
+  let(:bot) { described_class.new }
+
+  context 'when notifications are configured' do
+    let(:stub_client) { instance_spy('Slack::Web::Client') }
+
+    before do
+      allow(bot).to receive_messages(client: stub_client)
+    end
+
+    describe '#message' do
+      subject(:message) { bot.message(text: 'Test text') }
+
+      it 'sends text through to the client' do
+        message
+        expect(stub_client).to have_received(:chat_postMessage).with(hash_including(text: 'Test text'))
+      end
+
+      it 'allows defaults to be overridden' do
+        bot.message(text: 'Test text', channel: '#new-channel', as_user: false)
+
+        expect(stub_client).to have_received(:chat_postMessage).with(
+          hash_including(channel: '#new-channel', as_user: false)
+        )
+      end
+
+      it 'has a default channel and as_user option' do
+        message
+        expect(stub_client).to have_received(:chat_postMessage).with(
+          hash_including(channel: '#spotlight-service-team', as_user: true)
+        )
+      end
+    end
+  end
+
+  describe 'ExhibitBot::NullClient' do
+    it 'is returned when slack notification are not configured' do
+      expect(bot.send(:client)).to be_a ExhibitBot::NullClient
+    end
+  end
+end
