778d97c8b11814be992bcb56babed623e3b04768
Jessie Keck <jessie.keck@gmail.com>
2017-12-03 22:19:49 -0800

    Add an after_update callback to Exhibits that queues a job to trigger an ExhibitBot notification.

---
+++ spec/jobs/send_publish_state_change_notification_job_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SendPublishStateChangeNotificationJob do
+  let(:job) { described_class.perform_now(exhibit: exhibit, published: published) }
+  let(:exhibit) { FactoryBot.create(:exhibit) }
+
+  context 'when publishing' do
+    let(:published) { true }
+
+    it 'sends a message indicating the exhbiti has been pusblshed' do
+      allow(ExhibitBot).to receive(:message)
+
+      job
+
+      expect(ExhibitBot).to have_received(:message).with(
+        a_hash_including(
+          text: a_string_including("Spotlight exhibit published: #{exhibit.title}")
+        )
+      )
+    end
+  end
+
+  context 'when un-publishing' do
+    let(:published) { false }
+
+    it 'indicates an item is un-published' do
+      allow(ExhibitBot).to receive(:message)
+
+      job
+
+      expect(ExhibitBot).to have_received(:message).with(
+        a_hash_including(
+          text: a_string_including("Spotlight exhibit un-published: #{exhibit.title}")
+        )
+      )
+    end
+  end
+end
