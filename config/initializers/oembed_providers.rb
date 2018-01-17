18732da5633846158a8514e2de867c23d4afed56
Jessie Keck <jkeck@stanford.edu>
2018-01-17 14:23:38 -0800

    Revert "Allow for exhibit specific manifest URL patterns to be used for our oembed tag. (#1042)" (#1049)
    This reverts commit 23ce6116afd67ae98be846efbaeaa2fa576f7268.

--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -6,9 +6,4 @@ purl_provider = OEmbed::Provider.new('http://purl.stanford.edu/embed.{format}?&h
 purl_provider << 'http://purl.stanford.edu/*'
 purl_provider << 'https://purl.stanford.edu/*'
 purl_provider << 'http://searchworks.stanford.edu/*'
-
-purl_uat_provider = OEmbed::Provider.new('http://sul-purl-uat.stanford.edu/embed.{format}?&hide_title=true')
-purl_uat_provider << 'https://sul-purl-uat.stanford.edu/*'
-
 OEmbed::Providers.register(purl_provider)
-OEmbed::Providers.register(purl_uat_provider)
