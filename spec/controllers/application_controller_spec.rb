baa650c8f008ffb433235fb224ab7a9142dcd2a0
Jack Reed <phillipjreed@gmail.com>
2018-04-17 16:23:52 -0700

    refactor ApplicationController test to allow  for current_exhibit to be available

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,14 +4,15 @@ require 'rails_helper'
 
 describe ApplicationController do
   describe '#feature_flags' do
+    before do
+      allow(subject).to receive(:current_exhibit)
+        .and_return(FactoryBot.create(:exhibit, slug: 'test-flag-exhibit-slug'))
+    end
     it 'initializes a FeatureFlags object' do
-      expect(described_class.new.feature_flags).to be_a FeatureFlags
+      expect(controller.feature_flags).to be_a FeatureFlags
     end
 
     it 'sets current_exhibit context in the FeatureFlags object' do
-      controller = described_class.new
-      allow(controller).to receive(:current_exhibit).and_return(create(:exhibit, slug: 'test-flag-exhibit-slug'))
-
       expect(controller.feature_flags.test_thing?).to be true
     end
   end
