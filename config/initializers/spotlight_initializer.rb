43eec07f4954a5c6b7e49e39898ad6a0a863303c
Jack Reed <phillipjreed@gmail.com>
2018-04-24 08:34:52 -0600

    add needed form field types to upload form

--- config/initializers/spotlight_initializer.rb
+++ config/initializers/spotlight_initializer.rb
@@ -1,8 +1,10 @@
-Spotlight::Engine.config.upload_title_field = OpenStruct.new(field_name: 'title', solr_field: %w(title_full_display title_display title_245_search title_sort))
+Spotlight::Engine.config.upload_title_field = OpenStruct.new(
+  field_name: 'title', solr_field: %w(title_full_display title_display title_245_search title_sort), form_field_type: :text_field
+)
 Spotlight::Engine.config.upload_fields = [
   OpenStruct.new(field_name: Spotlight::Engine.config.upload_description_field, label: 'Description', form_field_type: :text_area),
-  OpenStruct.new(field_name: :spotlight_upload_attribution_tesim, label: 'Attribution'),
-  OpenStruct.new(field_name: 'date', solr_field: %w(spotlight_upload_date_tesim date_sort), label: 'Date')
+  OpenStruct.new(field_name: :spotlight_upload_attribution_tesim, label: 'Attribution', form_field_type: :text_field),
+  OpenStruct.new(field_name: 'date', solr_field: %w(spotlight_upload_date_tesim date_sort), label: 'Date', form_field_type: :text_field)
 ]
 Spotlight::Engine.config.default_contact_email = Settings.default_contact_email
 Spotlight::Engine.config.external_resources_partials += ['dor_harvester/form', 'bibliography_resources/form']
