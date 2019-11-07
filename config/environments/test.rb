4db5c69e1ba98cef644ee946a105060ca958808e
Camille Villa <cvilla100@gmail.com>
2019-11-06 16:40:54 -0800

    Update i18n fallbacks config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  # config.i18n.fallbacks = [I18n.default_locale]
+
   # Have long-running requests timeout after 60 seconds
   config.slowpoke.timeout = 60
 end
