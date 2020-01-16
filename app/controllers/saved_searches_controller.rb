c618fe650b8c230a1db427a72122f9bb10de46fa
Jack Reed <phillipjreed@gmail.com>
2020-01-16 09:49:10 -0700

    Remove savedsearches include

--- app/controllers/saved_searches_controller.rb
+++ app/controllers/saved_searches_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class SavedSearchesController < ApplicationController #:nodoc:
-  include Blacklight::SavedSearches
-
   helper BlacklightAdvancedSearch::RenderConstraintsOverride
 end
