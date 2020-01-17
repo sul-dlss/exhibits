eb2099953d7b9c91e2e2a723686182696520fb34
Chris Beer <cabeer@stanford.edu>
2020-01-17 08:30:54 -0800

    Use the bootstrap 4 way of attaching carets

--- app/assets/javascripts/table_of_contents.js
+++ app/assets/javascripts/table_of_contents.js
@@ -4,8 +4,6 @@ Blacklight.onLoad(function() {
   $('.blacklight-toc_search').on('show.bs.collapse hide.bs.collapse', function(){
     var _this = $(this);
     var linkText = $(_this).find('span');
-
-    $(_this).find('.caret').toggleClass('caret-right');
     $(linkText).text(linkText.text() == 'Show' ? 'Hide' : 'Show' );
   });
 });
