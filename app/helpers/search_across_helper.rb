50ead2f5aafbb13075565fd056cb1023ae99015a
Chris Beer <cabeer@stanford.edu>
2020-02-11 08:15:36 -0800

    Add extended pagination info for items and groups

--- app/helpers/search_across_helper.rb
+++ app/helpers/search_across_helper.rb
@@ -79,4 +79,52 @@ module SearchAcrossHelper
   def render_exhibit_title_facet(value)
     exhibit_metadata.slice(*value).values.map { |x| x['title'] || x['slug'] }.join(', ')
   end
+
+  ##
+  # Override the Kaminari page_entries_info helper with our own, blacklight-aware
+  # implementation. Why do we have to do this?
+  #  - We need custom counting information for grouped results
+  #  - We need to provide number_with_delimiter strings to i18n keys
+  # If we didn't have to do either one of these, we could get away with removing
+  # this entirely.
+  #
+  # @param [RSolr::Resource] collection (or other Kaminari-compatible objects)
+  # @return [String]
+  def page_entries_info(collection, entry_name: nil)
+    entry_name = if entry_name
+                   entry_name.pluralize(collection.size, I18n.locale)
+                 else
+                   collection.entry_name(count: collection.size).to_s.downcase
+                 end
+
+    entry_name = entry_name.pluralize unless collection.total_count == 1
+
+    # grouped response objects need special handling
+    end_num = if collection.respond_to?(:groups) && render_grouped_response?(collection)
+                collection.groups.length
+              else
+                collection.limit_value
+              end
+
+    end_num = if collection.offset_value + end_num <= collection.total_count
+                collection.offset_value + end_num
+              else
+                collection.total_count
+              end
+
+    case collection.total_count
+      when 0
+        t('search_across.pagination_info.no_items_found', entry_name: entry_name).html_safe
+      when 1
+        t('search_across.pagination_info.single_item_found', entry_name: entry_name).html_safe
+      else
+        t('search_across.pagination_info.pages', entry_name: entry_name,
+                                                     current_page: collection.current_page,
+                                                     num_pages: collection.total_pages,
+                                                     start_num: number_with_delimiter(collection.offset_value + 1),
+                                                     end_num: number_with_delimiter(end_num),
+                                                     total_num: number_with_delimiter(collection.total_count),
+                                                     count: collection.total_pages).html_safe
+    end
+  end
 end
