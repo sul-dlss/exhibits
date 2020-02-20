a747a66b7cf960aaf51a3d36acd2e076d088557e
Jack Reed <phillipjreed@gmail.com>
2020-02-20 06:58:26 -0700

    Add feature flag for search across form fixes #1715

--- spec/views/shared/_site_sidebar.html.erb_spec.rb
+++ spec/views/shared/_site_sidebar.html.erb_spec.rb
@@ -7,19 +7,20 @@ describe 'shared/_site_sidebar', type: :view do
 
   before do
     view.extend Spotlight::CrudLinkHelpers
-    stub_template 'catalog/search_sidebar' => 'search sidebar'
-    stub_template 'shared/_facets' => 'facets'
+    stub_template 'shared/_facets' => 'rendered facets'
   end
 
   context 'with a non-admin user' do
     before do
       allow(view).to receive(:can?).with(:create, Spotlight::Exhibit).and_return(false)
       allow(view).to receive(:can?).with(:manage, Spotlight::Site.instance).and_return(false)
+      allow(view).to receive(:current_page?).and_return(false)
       render
     end
 
     it { is_expected.to have_link('Request an exhibit') }
     it { is_expected.not_to have_link('Create a new exhibit') }
+    it { is_expected.to have_content 'rendered facets' }
   end
 
   context 'with an admin user' do
