7652d1b5364f617a7d31f2bff23815f087412418
Chris Beer <cabeer@stanford.edu>
2020-04-01 08:57:54 -0700

    Update server names

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,6 @@
 server 'exhibits-prod-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
 server 'exhibits-prod-b.stanford.edu', user: 'exhibits', roles: %w(web app)
-server 'exhibits-worker-prod-a.stanford.edu', user: 'exhibits', roles: %w(app background)
+server 'exhibits-worker-prod.stanford.edu', user: 'exhibits', roles: %w(app background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,6 +1,6 @@
 server 'exhibits-stage-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
 server 'exhibits-stage-b.stanford.edu', user: 'exhibits', roles: %w(web app)
-server 'exhibits-worker-stage-a.stanford.edu', user: 'exhibits', roles: %w(app background)
+server 'exhibits-worker-stage.stanford.edu', user: 'exhibits', roles: %w(app background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
