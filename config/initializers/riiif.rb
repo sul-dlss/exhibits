c95b06cc39180951aef611297143d5a5f8b85d47
Jessie Keck <jessie.keck@gmail.com>
2020-07-22 11:52:06 -0700

    Run new spotlight initializers.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
 
 # Riiif::Image.authorization_service = IIIFAuthorizationService
 
 # Riiif.not_found_image = 'app/assets/images/us_404.svg'
 #
-Riiif::Engine.config.cache_duration_in_days = 365
+Riiif::Engine.config.cache_duration = 365.days
