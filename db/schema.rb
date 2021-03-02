5771920c6f9977d837c3f7f9740dbf95b919bcf9
Chris Beer <chris@cbeer.info>
2021-03-02 12:20:46 -0800

    Correct the events migration

--- db/schema.rb
+++ db/schema.rb
@@ -159,7 +159,6 @@ ActiveRecord::Schema.define(version: 2021_02_11_221044) do
   end
 
   create_table "spotlight_events", force: :cascade do |t|
-    t.string "exhibit_type", null: false
     t.integer "exhibit_id", null: false
     t.string "resource_type", null: false
     t.integer "resource_id", null: false
@@ -168,7 +167,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_221044) do
     t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["exhibit_type", "exhibit_id"], name: "index_spotlight_events_on_exhibit_type_and_exhibit_id"
+    t.index ["exhibit_id"], name: "index_spotlight_events_on_exhibit_id"
     t.index ["resource_type", "resource_id"], name: "index_spotlight_events_on_resource_type_and_resource_id"
   end
 
