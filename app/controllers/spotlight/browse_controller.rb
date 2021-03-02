0b6cc8a006e2092935bb4d9972d39b4796c03548
Jack Reed <mejackreed@users.noreply.github.com>
2021-03-02 15:41:30 -0700

    Update app/controllers/spotlight/browse_controller.rb
    Co-authored-by: Chris Beer <cabeer@stanford.edu>

--- app/controllers/spotlight/browse_controller.rb
+++ app/controllers/spotlight/browse_controller.rb
@@ -5,7 +5,7 @@ spotlight_path = Gem::Specification.find_by_name('blacklight-spotlight').full_ge
 require_dependency File.join(spotlight_path, 'app/controllers/spotlight/browse_controller')
 
 module Spotlight
-  # Override the upstream HomePagesController in order to inject range limit behaviors
+  # Override the upstream controller to disable bulk actions.
   class BrowseController
     def render_bulk_actions?
       false
