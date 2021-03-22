d06951f308602175d999ea41b943423acf29e1b7
Camille Villa <cvilla100@gmail.com>
2021-03-22 15:19:22 -0700

    Use first canvas for external IIIF without IIIF service thumbnails

--- app/models/spotlight/resources/iiif_manifest.rb
+++ app/models/spotlight/resources/iiif_manifest.rb
@@ -20,19 +20,26 @@ module Spotlight
       def add_thumbnail_url
         return unless thumbnail_field
 
-        image = manifest['thumbnail'] if manifest['thumbnail'].present?
+        # if a thumbnail service is available
+        image = manifest['thumbnail'] if iiif_thumbnail_service?(manifest['thumbnail'])
+
+        # use the first canvas
         image ||= manifest.dig('sequences', 0, 'canvases', 0, 'images', 0, 'resource')
 
         return unless image
 
-        iiif_image = image['service']
-        if iiif_image['profile']&.match? 'api/image'
+        iiif_image = image['service'] || {}
+        if iiif_thumbnail_service?(image)
           solr_hash[:thumbnail_square_url_ssm] =
             "#{iiif_image['@id']}/full/100,100/0/default.jpg"
           solr_hash[:large_image_url_ssm] =
             "#{iiif_image['@id']}/full/!1000,1000/0/default.jpg"
         end
         solr_hash[thumbnail_field] = image['@id'] || "#{iiif_image['@id']}/full/!400,400/0/default.jpg"
+      end
+
+      def iiif_thumbnail_service?(content_resource)
+        content_resource&.dig('service', 'profile')&.match? 'api/image'
       end
     end
   end
