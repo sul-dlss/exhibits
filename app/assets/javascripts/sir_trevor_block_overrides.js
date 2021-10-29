a7b3cb01663a0754516180d99d6aca662f20a156
Chris Beer <cabeer@stanford.edu>
2021-10-29 12:13:05 -0700

    Override SirTrevor to work around a niche chrome-specific bug (#2139)

--- app/assets/javascripts/sir_trevor_block_overrides.js
+++ app/assets/javascripts/sir_trevor_block_overrides.js
@@ -8,3 +8,9 @@ SirTrevor.Blocks.SolrDocumentsEmbed = (function(){
     }
   });
 })();
+
+// work around for https://bugs.chromium.org/p/chromium/issues/detail?id=1262589&q=contenteditable&can=1
+if (navigator.userAgentData && navigator.userAgentData.brands &&
+    Boolean(navigator.userAgentData.brands.find(function(b) { return b.brand === 'Chromium' && parseFloat(b.version, 10) >= 95 && parseFloat(b.version, 10) < 97; }))) {
+  SirTrevor.Blocks.Text.prototype.editorHTML = "<div class=\"st-text-block\" spellcheck=\"false\" contenteditable=\"true\"></div>";
+}
