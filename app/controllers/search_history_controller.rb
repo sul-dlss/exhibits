a05541a6e6b4a44fe3cb5743612cda1a68896b5b
Chris Beer <chris@cbeer.info>
2022-06-20 12:56:58 -0700

    Remove unused SearchHistoryController override
    (cherry picked from commit 543e3e451e00a20b6dd0c361547d1cd64eafaae3)

--- app/controllers/search_history_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SearchHistoryController < ApplicationController # :nodoc:
-  include Blacklight::SearchHistory
-
-  helper BlacklightAdvancedSearch::RenderConstraintsOverride
-  helper BlacklightRangeLimit::ViewHelperOverride
-  helper RangeLimitHelper
-end
