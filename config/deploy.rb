827ffed2e7769ad33ce2ad69e6c7d31ba8accf56
Chris Beer <chris@cbeer.info>
2022-06-28 13:23:47 -0700

    Set the rails environment before clearing the cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       within release_path do
-        execute :rake, 'tmp:cache:clear'
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'tmp:cache:clear'
+        end
       end
     end
   end
