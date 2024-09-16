b9941fe0f0b5b22121a35c579eec7f924d69c2d5
Chris Beer <cabeer@stanford.edu>
2024-09-16 13:02:10 -0700

    Update existing exhibits to hide new fields by default.

---
+++ db/migrate/20240916120405_update_blacklight_config_defaults.rb
@@ -0,0 +1,20 @@
+class UpdateBlacklightConfigDefaults < ActiveRecord::Migration[7.2]
+  def up
+    Spotlight::BlacklightConfiguration.reset_column_information
+
+    Spotlight::BlacklightConfiguration.find_each do |config|
+      config.update(
+        facet_fields: config.facet_fields.reverse_merge({
+          'name_ssim' => { 'show' => false },
+          'name_roles_ssim' => { 'show' => false }
+        })
+      ) unless config.facet_fields.blank?
+
+      config.update(
+        index_fields: config.facet_fields.reverse_merge({
+          'name_roles_ssim' => { 'enabled' => false, 'show' => false }
+        })
+      ) unless config.index_fields.blank?
+    end
+  end
+end
