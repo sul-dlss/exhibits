7c22f4d447afb9e76b5c36d768a5aa28465e6005
dnoneill <noneill@stanford.edu>
2024-09-24 17:48:57 -0400

    fix flaky test, add SearchBarComponent inheritance

--- app/components/site_search_form_component.rb
+++ app/components/site_search_form_component.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
 # Builds the site search form
-class SiteSearchFormComponent < ViewComponent::Base
-  def presenter
-    Blacklight::SearchBarPresenter.new(controller, SearchAcrossController.blacklight_config)
-  end
-
-  def search_fields
-    @search_fields ||= helpers.blacklight_config.search_fields.values
-                              .select { |field_def| helpers.should_render_field?(field_def) }
-                              .collect do |field_def|
-      [helpers.label_for_search_field(field_def.key),
-       field_def.key]
-    end
-  end
+class SiteSearchFormComponent < Blacklight::SearchBarComponent
 end
