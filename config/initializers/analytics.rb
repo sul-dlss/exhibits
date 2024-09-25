eb3279ee5dd8140c7909408d98b478130fffdc26
dnoneill <noneill@stanford.edu>
2024-09-25 17:31:41 -0400

    update analytics initializer for ga4

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,6 +1,8 @@
 if Settings.ga
-  Spotlight::Engine.config.ga_pkcs12_key_path = Settings.ga.pkcs12_key_path
-  Spotlight::Engine.config.ga_email = Settings.ga.email
+  Spotlight::Engine.config.ga_json_key_path = Settings.ga.json_key_path
   Spotlight::Engine.config.ga_web_property_id = Settings.ga.web_property_id
+  Spotlight::Engine.config.ga_property_id = Settings.ga.property_id
   Spotlight::Engine.config.ga_debug_mode = Settings.analytics_debug
+  Spotlight::Engine.config.ga_page_analytics_options = Spotlight::Engine.config.ga_analytics_options.merge(limit: 5)
+  Spotlight::Engine.config.ga_search_analytics_options = Spotlight::Engine.config.ga_analytics_options.merge(limit: 11)
 end
