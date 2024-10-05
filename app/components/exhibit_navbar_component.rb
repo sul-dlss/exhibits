f1ac0f096ed40a0fa3497bcbc7265c0a67bbcb5d
Huda Khan <hudakhan@stanford.edu>
2024-10-04 18:28:35 -0600

    fixing rubocop errors

--- app/components/exhibit_navbar_component.rb
+++ app/components/exhibit_navbar_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Overrides Spotlight ExhibitNavbarComponent and provides Search Tips link
 class ExhibitNavbarComponent < Spotlight::ExhibitNavbarComponent
   def prepend_to_search_bar
     render SearchTipsLinkComponent.new
--- app/components/search_tips_link_component.rb
+++ app/components/search_tips_link_component.rb
@@ -2,4 +2,3 @@
 
 class SearchTipsLinkComponent < ViewComponent::Base
 end
-
