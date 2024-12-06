dc2a76d8a56e77206ea23efd0d671003c9565beb
Steve Taylor <sctaylor@stanford.edu>
2024-12-06 13:49:04 -0800

    Reflect the '--css bootstrap' changes from Spotlight

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
 import "@hotwired/turbo-rails"
-import bootstrap from "bootstrap"
+import "controllers"
+import * as bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
 import Spotlight from "spotlight"
