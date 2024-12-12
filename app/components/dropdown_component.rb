7c8593ba0cbd314f2c1d4c50e11f8922d7a080f4
Justin Coyne <jcoyne@justincoyne.com>
2024-12-12 15:27:32 -0600

    Set primary colors for group toggle

--- app/components/dropdown_component.rb
+++ app/components/dropdown_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+# Displays drop down for search controls. Overriding to use btn-outline-primary
 class DropdownComponent < Blacklight::System::DropdownComponent
   def before_render
     # this is overriding the default button classes set in Blacklight
     # (we want to use btn-outline-primary for EarthWorks instead of btn-outline-secondary)
-    with_button(label: button_label, classes: %w[btn btn-outline-primary dropdown-toggle])
+    with_button(label: button_label, classes: %w(btn btn-outline-primary dropdown-toggle))
     super
   end
 end
