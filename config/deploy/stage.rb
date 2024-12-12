bb181c322cd57b631cc10f103ff90c21fc7a28dc
Justin Coyne <jcoyne@justincoyne.com>
2024-12-12 10:48:16 -0600

    Don't try to restart passenger on stage worker nodes

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,7 +1,7 @@
 server 'exhibits-stage-a.stanford.edu', user: 'exhibits', roles: %w(web db app)
 server 'exhibits-stage-b.stanford.edu', user: 'exhibits', roles: %w(web app)
-server 'exhibits-worker-stage-a.stanford.edu', user: 'exhibits', roles: %w(app background)
-server 'exhibits-worker-stage-b.stanford.edu', user: 'exhibits', roles: %w(app background)
+server 'exhibits-worker-stage-a.stanford.edu', user: 'exhibits', roles: %w(background)
+server 'exhibits-worker-stage-b.stanford.edu', user: 'exhibits', roles: %w(background)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
