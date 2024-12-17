1df21a9c86e83f6c12ffdc7cb0f4b24ba5581814
dnoneill <noneill@stanford.edu>
2024-12-17 13:48:47 -0500

    settings for stage testing

--- config/initializers/oembed.rb
+++ config/initializers/oembed.rb
@@ -2,9 +2,11 @@ require 'oembed'
 
 OEmbed::Providers.register_all
 
-purl_provider = OEmbed::Provider.new('http://purl.stanford.edu/embed.{format}?&hide_title=true')
+#purl_provider = OEmbed::Provider.new('http://purl.stanford.edu/embed.{format}?&hide_title=true')
+purl_provider = OEmbed::Provider.new('https://sul-purl-stage.stanford.edu/embed.{format}?&hide_title=true')
 purl_provider << 'http://purl.stanford.edu/*'
 purl_provider << 'https://purl.stanford.edu/*'
+purl_provider << 'https://sul-purl-stage.stanford.edu/*'
 purl_provider << 'http://searchworks.stanford.edu/*'
 
 purl_uat_provider = OEmbed::Provider.new('https://sul-purl-uat.stanford.edu/embed.{format}?&hide_title=true')
