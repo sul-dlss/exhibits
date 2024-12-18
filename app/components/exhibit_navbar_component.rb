10cead08a76f326ffa742bb3be2d909982930a5b
Cory Lown <corylown@stanford.edu>
2024-12-18 10:41:55 -0500

    Do not render exhibit navbar if exhibit has configured search bar to not display

--- app/components/exhibit_navbar_component.rb
+++ app/components/exhibit_navbar_component.rb
@@ -2,4 +2,7 @@
 
 # Overrides Spotlight ExhibitNavbarComponent and provides Search Tips link
 class ExhibitNavbarComponent < Spotlight::ExhibitNavbarComponent
+  def render?
+    helpers.should_render_spotlight_search_bar?
+  end
 end
