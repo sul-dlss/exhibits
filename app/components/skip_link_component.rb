7f965152b25eb4f483da2fc6d59a0da9816c6a40
Huda Khan <hudakhan@stanford.edu>
2024-12-18 17:01:31 -0700

    changing to relative positioning should be sufficient

--- app/components/skip_link_component.rb
+++ app/components/skip_link_component.rb
@@ -2,4 +2,8 @@
 
 # Overriding the skip link component to use the component library classes
 class SkipLinkComponent < Blacklight::SkipLinkComponent
+  # This is only needed until we are using https://github.com/projectblacklight/blacklight/pull/3461
+  def link_classes
+    'visually-hidden-focusable rounded-bottom py-2 px-3'
+  end
 end
