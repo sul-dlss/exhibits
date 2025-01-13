f2053bcfa314bad67374ab363ac4e08b5c20106f
Huda Khan <hudakhan@stanford.edu>
2025-01-13 16:43:31 -0700

    overriding button styling

---
+++ app/components/facet_field_pagination_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Override the default Blacklight styling for pagination buttons
+class FacetFieldPaginationComponent < Blacklight::FacetFieldPaginationComponent
+end
