7e36a20151a5af5722169471dbe66ffe76e9ba49
Steve Taylor <sctaylor@stanford.edu>
2025-01-22 09:34:33 -0800

    Be more specific about BL oembed

--- app/javascript/blacklight_oembed.js
+++ app/javascript/blacklight_oembed.js
@@ -1,4 +1,4 @@
-import oembed from 'blacklight-oembed'
+import oembed from 'blacklight-oembed/app/assets/javascripts/blacklight_oembed/oembed.esm'
 
 Blacklight.onLoad(function() {
   oembed(document.querySelectorAll('[data-embed-url]'));
