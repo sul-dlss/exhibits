cee7ab327cb13c542dd0cbedf2205a7b7fad1927
Steve Taylor <sctaylor@stanford.edu>
2025-01-21 16:11:28 -0800

    Import the blacklight-gallery module

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,13 +13,12 @@ import githubAutoCompleteElement from "@github/auto-complete-element";
 
 import './bibliography'
 import './cited_documents'
-import 'blacklight-gallery/vendor/assets/javascripts/imagesloaded.pkgd.js'
-import 'blacklight-gallery/app/assets/javascripts/blacklight_gallery/slideshow'
-import 'blacklight-gallery/app/assets/javascripts/blacklight_gallery/masonry'
+
+import 'blacklight-gallery.esm.js'
+// Swap above for below after https://github.com/projectblacklight/blacklight-gallery/pull/176
+// import 'blacklight-gallery/blacklight-gallery.esm.js'
 
 import './blacklight_heatmaps'
-
-
 import './blacklight_oembed'
 import './full_text_collapse'
 import './index_status_typeahead'
