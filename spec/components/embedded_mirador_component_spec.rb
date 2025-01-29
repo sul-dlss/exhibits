66b4c7f43810c8aac460bc7e7d2c2bae3618f1f0
Justin Coyne <jcoyne@justincoyne.com>
2025-01-29 11:23:04 -0600

    Extract a component for embedded mirador

--- spec/components/embedded_mirador_component_spec.rb
+++ spec/components/embedded_mirador_component_spec.rb
@@ -2,22 +2,17 @@
 
 require 'rails_helper'
 
-describe 'catalog/_embedded_mirador3' do
+RSpec.describe EmbeddedMiradorComponent, type: :component do
+  let(:component) { described_class.new(document:) }
   let(:document) { SolrDocument.new(id: 'abc', iiif_manifest_url_ssi: manifest_url) }
   let(:manifest_url) { 'http://example.com/iiif/manifest' }
 
   before do
-    without_partial_double_verification do
-      allow(view).to receive_messages(
-        document: document
-      )
-    end
+    render_inline(component)
   end
 
   it 'renders an iframe' do
-    render
-
-    expect(rendered).to have_css "iframe[src='https://embed.stanford.edu/iiif?#{{ url: manifest_url }.to_query}']"
+    expect(page).to have_css "iframe[src='https://embed.stanford.edu/iiif?#{{ url: manifest_url }.to_query}']"
   end
 
   context 'with a local IIIF manifest' do
@@ -26,9 +21,7 @@ describe 'catalog/_embedded_mirador3' do
     it 'uses the full url to the manifest' do
       expected_url = 'http://test.host/iiif/manifest'
 
-      render
-
-      expect(rendered).to have_css "iframe[src='https://embed.stanford.edu/iiif?#{{ url: expected_url }.to_query}']"
+      expect(page).to have_css "iframe[src='https://embed.stanford.edu/iiif?#{{ url: expected_url }.to_query}']"
     end
   end
 end
