b977a9f2d0331d2958be0c92eceb5429e3e52c40
Justin Coyne <jcoyne@justincoyne.com>
2025-01-29 18:50:54 -0600

    Extract a OembedDefaultCompnent

--- app/components/custom_viewer_component.rb
+++ app/components/custom_viewer_component.rb
@@ -13,4 +13,38 @@ class CustomViewerComponent < Blacklight::Component
     @view_config = view_config
     @block_context = block_context
   end
+
+  def required_viewer
+    helpers.current_exhibit.required_viewer
+  end
+
+  ##
+  # Renders a viewer for an object with understanding of the context. In the
+  # context of spotlight/catalog render the configured viewer. In other contexts
+  # (feature page) render the default viewer. Now passes through a "block" from
+  # SirTrevor, used for rendering viewers in specific ways (canvas index).
+  # @param [SolrDocument] document
+  # @param [SirTrevorRails::Blocks::SolrDocumentsEmbedBlock] block
+  def render_viewer_in_context
+    canvas = choose_canvas_id
+    partial = if params[:controller] == 'spotlight/catalog'
+                required_viewer.to_partial_path
+              else
+                required_viewer.default_viewer_path
+              end
+
+    render partial:, locals: { document:, block: block_context, canvas:, oembed_url: }
+  end
+
+  ##
+  # This method sends the message of which IIIF canvas should be selected by the sul-embed viewer.
+  # @param [SirTrevorRails::Blocks::SolrDocumentsEmbedBlock] block
+  # @return [String] Selected canvas URI
+  def choose_canvas_id
+    block_context&.items&.dig(0, 'iiif_canvas_id') if block_context.respond_to? :items
+  end
+
+  def oembed_url
+    @oembed_url ||= helpers.context_specific_oembed_url(document)
+  end
 end
