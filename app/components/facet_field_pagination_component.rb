6dd3737ed4d48711b1b1828a6a3a712e76862874
Huda Khan <hudakhan@stanford.edu>
2025-02-04 16:10:41 -0700

    using latest blacklight release to override facet button colors

--- app/components/facet_field_pagination_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Override the default Blacklight styling for pagination buttons
-class FacetFieldPaginationComponent < Blacklight::FacetFieldPaginationComponent
-end
