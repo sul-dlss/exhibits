309b0689aae0be10a39fe3217b45820a78fb0cac
Steve Taylor <sctaylor@stanford.edu>
2025-02-19 16:08:57 -0800

    Test links rather than styles

--- spec/components/skip_link_component_spec.rb
+++ spec/components/skip_link_component_spec.rb
@@ -4,24 +4,21 @@ require 'rails_helper'
 
 RSpec.describe SkipLinkComponent, type: :component do
   describe 'no search results' do
-    before do
-      render_inline(described_class.new)
+    it 'renders skip links with correct link to search' do
+      rendered = render_inline(described_class.new)
+      expect(rendered).to have_link('Skip to main content', href: '#main-container')
+      expect(rendered).to have_link('Skip to search', href: '#search_field')
     end
-
-    it { expect(page).to have_css('#skip-link-exhibits.visually-hidden-focusable') }
-    it { expect(page).to have_css('a.d-inline-flex.m-1', visible: false, count: 2) }
   end
 
   # If an existing content block is passed to this component,
   # the component should include the link to the first result
   describe 'with existing search results' do
-    before do
-      render_inline(described_class.new) do
-        'something'
-      end
+    it 'renders skip links with correct links to search and results' do
+      rendered = render_inline(described_class.new) { 'some content' }
+      expect(rendered).to have_link('Skip to main content', href: '#main-container')
+      expect(rendered).to have_link('Skip to search', href: '#search_field')
+      expect(rendered).to have_link('Skip to first result', href: '#documents')
     end
-
-    it { expect(page).to have_css('a.d-inline-flex.m-1', visible: false, count: 3) }
-    it { expect(page).to have_link(href: '#documents') }
   end
 end
