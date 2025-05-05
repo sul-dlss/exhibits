0e9b512a83b917a199e43790bf817210f54bd8de
Peter Mangiafico <peter@mangiafico.org>
2025-05-05 11:25:21 -0700

    add extra columsn to output header

--- lib/alt_text.py
+++ lib/alt_text.py
@@ -16,20 +16,24 @@ from vertexai.generative_models import (
     HarmBlockThreshold,
 )
 
+#################
+# CONFIGURATION
 # Define project information
+BASE_IMAGE_URL = "https://exhibits.stanford.edu"
 PROJECT_ID = "sul-ai-sandbox"  # @param {type:"string"}
 LOCATION = "us-central1"  # @param {type:"string"}
 BUCKET_NAME = "cloud-ai-platform-e215f7f7-a526-4a66-902d-eb69384ef0c4"
-BASE_IMAGE_URL = "https://exhibits.stanford.edu"
-DIRECTORY = "exhibits-alt-text/pilot-2"
-INPUTFILE = f'{DIRECTORY}/images.csv'
-OUTPUTFOLDER = f'{DIRECTORY}/output'
+DIRECTORY = "exhibits-alt-text/pilot-2" # base directory for the images and CSV file in the google bucket
+INPUTFILE = f'{DIRECTORY}/images.csv' # location of the input CSV file in the google bucket
+IMAGES_FOLDER = f'{DIRECTORY}/images' # location of the images in the google bucket
+OUTPUT_FOLDER = f'{DIRECTORY}/output' # location of the where to write the output CSV files in the google bucket
+LOGFILE = 'log.txt' # location of the log file in Vertex AI
 MODEL_ID = 'gemini-2.0-flash'
 # other options (see Google AI Studio for more):
 # "gemini-2.5-pro-preview-03-25"
 # "gemini-1.5-pro"
 TEST_LIMIT = None # limit the number of images to process for testing, set to None to process all
-DEBUG = False # set to True to show the prompts in the output and NOT send images to AI (test script only)
+DEBUG = False # set to True to NOT send images to AI (test script only)
 ONLY_USE_EXHIBIT_NAME = None # "Digitization Exemplars" # limit to this exhibit name, set to None to process all
 
 safety_settings = {
@@ -46,12 +50,18 @@ model = GenerativeModel(
 
 # Initialize Vertex AI
 vertexai.init(project=PROJECT_ID, location=LOCATION)
+#################
 
 
+#################
+# SHARED FUNCTIONS
 # Send Google Cloud Storage Document to Vertex AI
 def generate_description(
     prompt: str,
     file_uri: str,
+    exhibit_name: str,
+    page_title: str,
+    image_url: str,
     generation_config: GenerationConfig | None = None,
 ) -> str:
     # Load file directly from Google Cloud Storage
@@ -65,16 +75,15 @@ def generate_description(
 
     try:
       # Send to Gemini
-      print(f"...sending to {MODEL_ID}")
       response = model.generate_content(contents)#, generation_config=generation_config)
 
       return response.text.rstrip()
-    except ValueError as e:
-      # Handle the ValueError exception
-      print(f"A ValueError occurred: {e}")
     except Exception as e:
         # Handle any other unforeseen errors
-        print(f"An unexpected error occurred: {e}")
+        error = f"**** ERROR: {e}. Exhibit: {exhibit_name}.  Page: {page_title}.  Image: {image_url}"
+        print(error, file = f)
+        print("", file = f)
+        print(error)
 
 def full_image_url(image_url):
   # Convert the image_url to a full URL if it not already a full URL
@@ -107,79 +116,105 @@ def generate_prompt(exhibit_name, exhibit_subtitle, exhibit_description, extra_t
 
 
 def output_csv_header():
-  return ["Exhibit", "Page Title", "Page URL", "Image URL", "Bucket Image Filename", f"AI Generated Description ({MODEL_ID})", "Prompt"]
+  return ["Exhibit", "Page Title", "Page URL", "Image URL", "Bucket Image Filename", f"AI Generated Description ({MODEL_ID})", "Prompt",
+          'AI description NOT useful and new, accurate alt text must be created (mark with an "X" if so)',
+          'AI description useful AS IS (mark with an "X" if so)',
+          'AI description partially useful with EDITS NEEDED (mark with an "X" if so)',
+          "New OR edited description (when needed)",
+          'Image is DECORATIVE, no description required (mark with an "X" if so)']
 
 
 def output_csv_filename(exhibit_name):
   fileslug = exhibit_name.replace(" ", "_").replace("/", "_").replace(":", "_").replace("'", "_").replace('"', "_").replace("?", "_").replace(",", "_").replace("&", "_")
-  return f"{OUTPUTFOLDER}/{fileslug}.csv"
+  return f"{OUTPUT_FOLDER}/{fileslug}.csv"
 
+
+def write_output_csv_file(exhibit_name, csv_buffer):
+  output_filename = output_csv_filename(exhibit_name)
+  print(f"Writing {exhibit_name} to {output_filename}")
+  csv_content = csv_buffer.getvalue()
+  get_blob(output_filename).upload_from_string(csv_content, content_type='text/csv')
+#################
+
+#################
+# Main script
 with get_blob(INPUTFILE).open() as csvfile:
-  print(f"Reading {INPUTFILE}")
-  print()
+  with open(LOGFILE, "w") as f:
+    output = f"Running {MODEL_ID} on {INPUTFILE} with {BUCKET_NAME}/{IMAGES_FOLDER}"
+    print(output, file = f)
+    print("", file = f)
+    print(output)
 
-  reader = csv.reader(csvfile)
+    reader = csv.reader(csvfile)
 
-  next(reader) # skip headers
-  previous_exhibit_name = None
+    next(reader) # skip headers
+    previous_exhibit_name = None
 
-  count = 0
-  csv_buffer = StringIO()
+    count = 0
+    csv_buffer = StringIO()
 
-  for row in reader:
-    exhibit_name = row[0]
+    for row in reader:
+      exhibit_name = row[0]
 
-    if ONLY_USE_EXHIBIT_NAME and exhibit_name != ONLY_USE_EXHIBIT_NAME:
-      print(f"Skipping {exhibit_name} because it does not match {ONLY_USE_EXHIBIT_NAME}")
-      continue
+      if ONLY_USE_EXHIBIT_NAME and exhibit_name != ONLY_USE_EXHIBIT_NAME:
+        print(f"Skipping {exhibit_name} because it does not match {ONLY_USE_EXHIBIT_NAME}")
+        continue
 
-    # Check if the exhibit name has changed
-    if exhibit_name != previous_exhibit_name:
-      if previous_exhibit_name is not None:
-        # Write out the previous CSV file if this is the first exhibit overall
-        output_filename = output_csv_filename(previous_exhibit_name)
-        print(f"Writing {previous_exhibit_name} to {output_filename}")
-        csv_content = csv_buffer.getvalue()
-        get_blob(output_filename).upload_from_string(csv_content, content_type='text/csv')
+      # Check if the exhibit name has changed
+      if exhibit_name != previous_exhibit_name:
+        if previous_exhibit_name is not None:
+          # Write out the previous CSV file if this is the first exhibit overall
+          write_output_csv_file(previous_exhibit_name, csv_buffer)
 
-      # Create a new CSV file
-      previous_exhibit_name = exhibit_name
-      print()
-      print(f"Processing {exhibit_name}")
-      # Create a new StringIO buffer for the new exhibit
-      csv_buffer = StringIO()
-      writer = csv.writer(csv_buffer)
-      writer.writerow(output_csv_header())
+        # Create a new CSV file
+        previous_exhibit_name = exhibit_name
+        print()
+        print('**********************************', file = f)
+        print(exhibit_name, file = f)
+        print("", file = f)
+        print(exhibit_name)
+        # Create a new StringIO buffer for the new exhibit
+        csv_buffer = StringIO()
+        writer = csv.writer(csv_buffer)
+        writer.writerow(output_csv_header())
 
-    count += 1
-    exhibit_description = row[1]
-    exhibit_subtitle = row[2]
-    page_title = row[3]
-    extra_text = row[4]
-    image_title = row[5]
-    image_caption = row[6]
-    # exhibit_slug = row[7]
-    # page_slug = row[8]
-    page_url = row[9]
-    image_url = full_image_url(row[10])
-    image_metadata = image_title + " " + image_caption
-    bucket_image_url = f"{count}.jpg"
-    file_uri = f'gs://{BUCKET_NAME}/{DIRECTORY}/{bucket_image_url}'
+      count += 1
+      exhibit_description = row[1]
+      exhibit_subtitle = row[2]
+      page_title = row[3]
+      extra_text = row[4]
+      image_title = row[5]
+      image_caption = row[6]
+      # exhibit_slug = row[7]
+      # page_slug = row[8]
+      page_url = row[9]
+      image_url = full_image_url(row[10])
+      image_metadata = image_title + " " + image_caption
+      bucket_image_url = row[11]
+      file_uri = f'gs://{BUCKET_NAME}/{IMAGES_FOLDER}/{bucket_image_url}'
 
-    print(f"{count} : {file_uri}")
+      log = f"Row {count} : {exhibit_name} : {bucket_image_url} : {file_uri}"
+      print(log, file = f)
+      print(f"Row {count}")
 
-    prompt = generate_prompt(exhibit_name, exhibit_subtitle, exhibit_description, extra_text, image_metadata)
+      prompt = generate_prompt(exhibit_name, exhibit_subtitle, exhibit_description, extra_text, image_metadata)
 
-    if DEBUG:
-      print(f"Prompt: {prompt}")
-      description = "Description from AI would go here"
-    else:
-      description = generate_description(prompt, file_uri)
+      print(f"Prompt: {prompt}", file = f)
+      print("", file = f)
 
-    writer.writerow([exhibit_name, page_title, page_url, image_url, bucket_image_url, description, prompt])
-    print()
-    if TEST_LIMIT and count >= TEST_LIMIT:
-      print(f"Test limit reached: {TEST_LIMIT}")
-      break
+      if DEBUG:
+        description = "Description from AI would go here"
+      else:
+        description = generate_description(prompt, file_uri, exhibit_name, page_title, image_url)
 
-print(f"Completed {count} rows")
+      writer.writerow([exhibit_name, page_title, page_url, image_url, bucket_image_url, description, prompt])
+
+      if TEST_LIMIT and count >= TEST_LIMIT:
+        print(f"Test limit reached: {TEST_LIMIT}")
+        break
+
+  # write out the last exhibit csv file
+  write_output_csv_file(exhibit_name, csv_buffer)
+
+  print(f"Completed {count} rows")
+  #################
--- lib/tasks/alt_text.rake
+++ lib/tasks/alt_text.rake
@@ -13,7 +13,7 @@ namespace :alt_text do
     base_url = 'https://exhibits.stanford.edu'
     Spotlight::Engine.routes.default_url_options[:host] = base_url
 
-    exhibits_filename = 'tmp/exhibits.csv' # a file with one line per exhibit title to export (no header)
+    exhibits_filename = 'tmp/exhibits.txt' # a file with one line per exhibit title to export (no header)
     output_filename = 'tmp/images.csv' # a file to write the output to
     output_image_folder = 'tmp/images' # a folder to write the images to
 
@@ -21,14 +21,19 @@ namespace :alt_text do
 
     exhibit_titles = File.readlines(exhibits_filename).map(&:strip)
     total_exhibits = exhibit_titles.size
-    puts "Exporting images from #{total_exhibits} exhibits listed in #{exhibits_filename} to #{output_filename}"
+    exhibits_exported_count = 0
+    puts "Exporting image metadata for exhibits to #{output_filename}"
+    puts "Found #{total_exhibits} exhibits listed in #{exhibits_filename}"
     exhibits = Spotlight::Exhibit.where(title: exhibit_titles)
     CSV.open(output_filename, 'wb') do |csv|
       csv << ['Exhibit', 'Exhibit description', 'Exhibit subtitle', 'Page title', 'Extra Text', 'image title',
-              'image caption', 'exhibit slug', 'page slug', 'page url', 'image url']
-      puts "Found #{exhibits.size} exhibits"
+              'image caption', 'exhibit slug', 'page slug', 'page url', 'image url', 'bucket filename']
+      puts "Found #{exhibits.size} exhibits in database"
+      puts ''
+
+      bucket_filename_counter = 1
       exhibits.each do |exhibit|
-        puts "Exporting images from exhibit: #{exhibit.title}"
+        images_exhibit_count = 0
         pages_with_alt = exhibit.pages.order(Arel.sql('id = 1 DESC, created_at DESC')).select do |elem|
           elem.content.any?(&:supports_alt_text?)
         end
@@ -44,26 +49,35 @@ namespace :alt_text do
             images = block.item || {}
             images_without_alt = images.values.select { |img| img['alt_text'].blank? && img['decorative'].blank? }
             images_without_alt.each do |img|
-              url = img['url'] || img['full_image_url'].presence ||
-                    img['iiif_tilesource'].sub('info.json', '/full/!400,400/0/default.jpg')
-              next if url.blank? || url == 'undefined' # Likely a media item. Not an image.
+              images_exhibit_count += 1
+              image_url = img['url'] || img['full_image_url'].presence ||
+                          img['iiif_tilesource'].sub('info.json', '/full/!400,400/0/default.jpg')
+              next if image_url.blank? || image_url == 'undefined' # Likely a media item. Not an image.
 
+              bucket_filename_counter += 1
               csv << [exhibit.title, exhibit.description, exhibit.subtitle, page.title, extra_text,
-                      img['title'], img['caption'], exhibit.slug, page.slug, page_url, url]
+                      img['title'], img['caption'], exhibit.slug, page.slug, page_url, image_url,
+                      "#{bucket_filename_counter}.jpg"]
             end
           end
         end
+        puts "Exported #{images_exhibit_count} images without alt text in exhibit: #{exhibit.title}"
+        exhibits_exported_count += 1 unless images_exhibit_count.zero?
       end
     end
+    puts ''
+    puts "Exported #{exhibits_exported_count} exhibits that contained at least one image without alt text"
 
+    puts ''
     puts "Downloading images from #{output_filename} to #{output_image_folder}"
     FileUtils.mkdir_p(output_image_folder)
 
-    CSV.foreach(output_filename, headers: true).with_index(1) do |row, n|
+    CSV.foreach(output_filename, headers: true).with_index(1) do |row, _n|
       url = row['image url']
+      filename = row['bucket filename']
       url = "#{base_url}#{url}" unless url.start_with?('https://')
-      puts "Exporting image #{n}: #{url}"
-      `curl -s #{url} -o #{output_image_folder}/#{n}.jpg`
+      puts "Exporting #{url} to #{filename}"
+      `curl -s #{url} -o #{output_image_folder}/#{filename}`
     end
   end
 end
