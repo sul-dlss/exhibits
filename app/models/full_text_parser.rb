6655631b231b7850bc942f2357a6d702f1b9efca
Cory Lown <corylown@stanford.edu>
2025-06-23 16:57:06 -0400

    Adapt FullTextParser to fetch file info from Cocina

--- app/models/full_text_parser.rb
+++ app/models/full_text_parser.rb
@@ -6,25 +6,36 @@
 # and can return an array of OCR strings (one element in the array for every OCR file).
 class FullTextParser
   attr_reader :druid, :purl_object
-  delegate :public_xml, to: :purl_object
+  delegate :public_cocina, to: :purl_object
 
+  TRANSCRIPTION_FILE_USE = 'transcription'
+  TRANSCRIPTION_FILE_MIMETYPES = %w(text/plain text/html application/xml application/alto+xml).freeze
+  TRANSCRIPTION_RESOURCE_TYPES = %w(https://cocina.sul.stanford.edu/models/resources/page
+                                    https://cocina.sul.stanford.edu/models/resources/image).freeze
+
+  XML_OCR_TYPES = %w(application/xml application/alto+xml).freeze
+  HTML_OCR_TYPES = %w(text/html).freeze
+
+  # @param purl_object [Purl] the Purl object containing the resources to parse
   def initialize(purl_object)
     @purl_object = purl_object
     @druid = purl_object.bare_druid
   end
 
+  # @return [Array<Hash>] an array of hashes representing the OCR files
   def ocr_files
-    @ocr_files ||= xpath.flat_map { |xp| public_xml.xpath(xp) }
+    @ocr_files ||= transcription_files.concat(text_transcription_files)
   end
 
   # TODO: Support .rtf (and .xml?) files, scrubbing/converting as necessary to get plain text
+  # @return [Array<String>] an array of strings representing the full text content of the OCR files
   def to_text
     ocr_files.map do |resource|
-      url = full_text_url(resource['id'])
+      url = full_text_url(resource.fetch('filename', ''))
       content = full_text_content(url)
-      if xml_ocr_types.include?(resource['mimetype'])
+      if XML_OCR_TYPES.include?(resource.fetch('hasMimeType', ''))
         alto_xml_string_content(content)
-      elsif html_ocr_types.include?(resource['mimetype'])
+      elsif HTML_OCR_TYPES.include?(resource.fetch('hasMimeType', ''))
         hocr_string_content(content)
       else # plain text
         content.scrub.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?').gsub(/\s+/, ' ')
@@ -34,18 +45,10 @@ class FullTextParser
 
   private
 
-  def xml_ocr_types
-    ['application/xml', 'application/alto+xml']
-  end
-
-  def html_ocr_types
-    ['text/html']
-  end
-
   def full_text_content(url)
     Faraday.get(url).body
   rescue Faraday::ConnectionFailed
-    logger.error("Error indexing full text - couldn't load file #{url}")
+    Rails.logger.error("Error indexing full text - couldn't load file #{url}")
     ''
   end
 
@@ -69,18 +72,36 @@ class FullTextParser
     hocr.css('.ocr_page').map(&:text).join(' ')
   end
 
-  # full text in druid.txt named for druid (feigenbaum) and ALTO OCR xml in page resources
-  # rubocop:disable Layout/LineLength
-  def xpath
-    [
-      "//contentMetadata/resource/file[@id=\"#{druid}.txt\"]",
-      "//contentMetadata/resource[@type='page']/file[@role='transcription'][@mimetype='text/plain' or @mimetype='text/html' or @mimetype='application/xml' or @mimetype='application/alto+xml']",
-      "//contentMetadata/resource[@type='image']/file[@role='transcription'][@mimetype='text/plain' or @mimetype='text/html' or @mimetype='application/xml' or @mimetype='application/alto+xml']"
-    ]
+  # Supports feigenbaum style transcription files
+  # where the filename is the druid with a .txt extension.
+  def text_transcription_files
+    public_cocina_contains.flat_map do |resource|
+      resource_contains(resource).select do |file|
+        file.fetch('filename', '') == "#{druid}.txt"
+      end
+    end
   end
-  # rubocop:enable Layout/LineLength
 
-  def logger
-    Rails.logger
+  def transcription_files
+    transcription_resources.flat_map do |resource|
+      resource_contains(resource).select do |file|
+        file.fetch('use', '') == TRANSCRIPTION_FILE_USE &&
+          file.fetch('hasMimeType').in?(TRANSCRIPTION_FILE_MIMETYPES)
+      end
+    end
+  end
+
+  def transcription_resources
+    public_cocina_contains.select do |resource|
+      resource.fetch('type', '').in?(TRANSCRIPTION_RESOURCE_TYPES)
+    end
+  end
+
+  def public_cocina_contains
+    resource_contains(public_cocina)
+  end
+
+  def resource_contains(resource)
+    Array(resource.dig('structural', 'contains'))
   end
 end
--- spec/models/full_text_parser_spec.rb
+++ spec/models/full_text_parser_spec.rb
@@ -5,21 +5,25 @@ require 'rails_helper'
 describe FullTextParser do
   subject(:parser) { described_class.new(purl_object) }
 
-  let(:purl_object) { double('PurlObject', bare_druid: 'cc842mn9348', public_xml: public_xml) }
-  let(:public_xml) { Nokogiri::XML.parse(File.read(File.join(FIXTURES_PATH, 'cc842mn9348.xml'))) }
+  let(:purl_object) { instance_double(Purl, bare_druid: 'cc842mn9348', public_cocina:) }
+  let(:public_cocina) { JSON.parse(File.read(File.join(FIXTURES_PATH, 'cc842mn9348.json'))) }
 
   describe '#ocr_files' do
+    subject(:ocr_files) { parser.ocr_files }
+
     it 'is an array of nokogiri elements for the appropriate files' do
-      ocr_files = parser.ocr_files
       expect(ocr_files.length).to eq 1
-      expect(ocr_files.first['mimetype']).to eq 'application/xml'
+      expect(ocr_files.first.fetch('hasMimeType')).to eq 'application/xml'
     end
   end
 
   describe '#to_text' do
+    subject(:ocr_text) { parser.to_text }
+
+    let(:fixture_file) { File.read(File.join(FIXTURES_PATH, 'cc842mn9348_ocr_1.xml')) }
+    let(:file_name) { 'EastTimor_CE-SPSC_Final_Decisions_2001_04b-2001_Sabino_Gouveia_Leite_Judgment_0001.xml' }
+
     before do
-      fixture_file = File.read(File.join(FIXTURES_PATH, 'cc842mn9348_ocr_1.xml'))
-      file_name = 'EastTimor_CE-SPSC_Final_Decisions_2001_04b-2001_Sabino_Gouveia_Leite_Judgment_0001.xml'
       stub_request(
         :get,
         "https://stacks.stanford.edu/file/cc842mn9348/#{file_name}"
@@ -27,73 +31,69 @@ describe FullTextParser do
     end
 
     it 'is an array of text parsed of the OCR files' do
-      ocr_text = parser.to_text
       expect(ocr_text.length).to eq 1
       expect(ocr_text.first).to start_with 'INTRODUCTION 1 The trial of'
       expect(ocr_text.first).to end_with 'with the rendering of the decision.'
     end
-  end
 
-  context 'with an hOCR transcription' do
-    let(:purl_object) { double('PurlObject', bare_druid: 'hocrexample', public_xml: public_xml) }
-    let(:public_xml) { Nokogiri::XML.parse(File.read(File.join(FIXTURES_PATH, 'hocrexample.xml'))) }
-    let(:text) do
-      <<-FIXTURE
-        <html>
-          <body>
-            <div class='ocr_page'><p class='ocr_par'><span class='ocrx_word'>asdf</span></p></div>
-          </body>
-        </html>
-      FIXTURE
-    end
+    context 'with an hOCR transcription' do
+      let(:purl_object) { instance_double(Purl, bare_druid: 'hocrexample', public_cocina:) }
+      let(:public_cocina) { JSON.parse(File.read(File.join(FIXTURES_PATH, 'hocrexample.json'))) }
+      let(:text) do
+        <<-FIXTURE
+          <html>
+            <body>
+              <div class='ocr_page'><p class='ocr_par'><span class='ocrx_word'>asdf</span></p></div>
+            </body>
+          </html>
+        FIXTURE
+      end
 
-    before do
-      stub_request(
-        :get,
-        %r{^https://stacks.stanford.edu/file/hocrexample/.*\.html$}
-      ).to_return(status: 200, body: text)
-    end
+      before do
+        stub_request(
+          :get,
+          %r{^https://stacks.stanford.edu/file/hocrexample/.*\.html$}
+        ).to_return(status: 200, body: text)
+      end
 
-    describe '#to_text' do
-      it 'is an array of text parsed from the hOCR text' do
-        ocr_text = parser.to_text
-        expect(ocr_text.length).to eq 15
-        expect(ocr_text.first).to eq 'asdf'
+      describe '#to_text' do
+        it 'is an array of text parsed from the hOCR text' do
+          expect(ocr_text.length).to eq 15
+          expect(ocr_text.first).to eq 'asdf'
+        end
       end
     end
-  end
 
-  context 'with a plain-text transcription' do
-    let(:purl_object) { double('PurlObject', bare_druid: 'xt162pg0437', public_xml: public_xml) }
-    let(:public_xml) { Nokogiri::XML.parse(File.read(File.join(FIXTURES_PATH, 'xt162pg0437.xml'))) }
+    context 'with a plain-text transcription' do
+      let(:purl_object) { instance_double(Purl, bare_druid: 'xt162pg0437', public_cocina:) }
+      let(:public_cocina) { JSON.parse(File.read(File.join(FIXTURES_PATH, 'xt162pg0437.json'))) }
 
-    before do
-      stub_request(
-        :get,
-        %r{^https://stacks.stanford.edu/file/xt162pg0437/.*\.txt$}
-      ).to_return(status: 200, body: 'asdf')
-    end
+      before do
+        stub_request(
+          :get,
+          %r{^https://stacks.stanford.edu/file/xt162pg0437/.*\.txt$}
+        ).to_return(status: 200, body: 'asdf')
+      end
 
-    describe '#to_text' do
-      it 'is an array of text parsed from the plain text fields' do
-        ocr_text = parser.to_text
-        expect(ocr_text.length).to eq 15
-        expect(ocr_text.first).to eq 'asdf'
+      describe '#to_text' do
+        it 'is an array of text parsed from the plain text fields' do
+          expect(ocr_text.length).to eq 15
+          expect(ocr_text.first).to eq 'asdf'
+        end
       end
     end
-  end
 
-  context 'when Stacks is down' do
-    before do
-      allow(Faraday).to receive(:get).and_raise(
-        Faraday::ConnectionFailed.new('Connection refused - connect(2) for "stacks.stanford.edu" port 443')
-      )
-    end
+    context 'when Stacks is down' do
+      before do
+        allow(Faraday).to receive(:get).and_raise(
+          Faraday::ConnectionFailed.new('Connection refused - connect(2) for "stacks.stanford.edu" port 443')
+        )
+      end
 
-    describe '#to_text' do
-      it 'is an empty array' do
-        ocr_text = parser.to_text.flatten
-        expect(ocr_text).to eq []
+      describe '#to_text' do
+        it 'is an empty array' do
+          expect(ocr_text.flatten).to eq []
+        end
       end
     end
   end
