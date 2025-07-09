d54bec18fb76f2ebd01ba54db711ad763dd4de9f
Cory Lown <corylown@stanford.edu>
2025-07-09 16:40:08 -0400

    Extract common dor traject configurations

--- lib/traject/macros/dor.rb
+++ lib/traject/macros/dor.rb
@@ -12,6 +12,22 @@ module Traject
         end
       end
 
+      def cocina_display(method, *args, **kwargs)
+        lambda do |resource, accumulator, _context|
+          Array(resource.cocina_record.public_send(method, *args, **kwargs)).each do |v|
+            accumulator << v
+          end
+        end
+      end
+
+      def cocina_display_path(path)
+        lambda do |resource, accumulator, _context|
+          Array(resource.cocina_record.path(path)).each do |v|
+            accumulator << v if v.present?
+          end
+        end
+      end
+
       def resource_images_iiif_urls
         lambda do |resource, accumulator, _context|
           next if resource.thumbnail_identifier.nil?
