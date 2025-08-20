2834f238e11bd4a0734caa2478e3d8a6c50fe084
Cory Lown <corylown@stanford.edu>
2025-08-20 08:58:02 -0400

    Set imagemagick commands for version 7

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -7,4 +7,9 @@ ActiveSupport::Reloader.to_prepare do
   # Riiif.not_found_image = 'app/assets/images/us_404.svg'
   #
   Riiif::Engine.config.cache_duration = 365.days
+
+  # ImageMagick 7 commands are prefixed with "magick". This is needed until
+  # we update the default commands set by the Riiif gem.
+  Riiif::ImagemagickCommandFactory.external_command = "magick convert"
+  Riiif::ImageMagickInfoExtractor.external_command  = "magick identify"
 end
