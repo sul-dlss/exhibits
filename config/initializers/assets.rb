6d6ed6eb742c6287dbfe42d79bc7ff15109fb87d
Steve Taylor <sctaylor@stanford.edu>
2025-12-11 13:20:36 -0800

    Update to Rails 8.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,3 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules/openseadragon/build/openseadragon/images')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w()
