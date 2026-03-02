680c2eccee50a0bcaaf3b524bd23c2d5e43765b1
Cory Lown <corylown@stanford.edu>
2026-03-02 13:15:09 -0500

    Use cocina display methods for displaying publisher metadata

--- app/components/metadata/cocina/description_component.rb
+++ app/components/metadata/cocina/description_component.rb
@@ -14,7 +14,8 @@ module Metadata
 
       delegate :form_display_data, :language_display_data,
                :form_note_display_data, :map_display_data,
-               :event_note_display_data, :event_date_display_data, to: :cocina_record
+               :event_note_display_data, :event_date_display_data,
+               :publication_display_data, to: :cocina_record
 
       def call
         render Metadata::SectionComponent.new(label: I18n.t('metadata.description')) do
@@ -31,8 +32,7 @@ module Metadata
       def description
         @description ||= [title_display_data,
                           form_display_data,
-                          publication_places,
-                          publisher,
+                          publication_display_data,
                           event_date_display_data,
                           event_note_display_data,
                           language_display_data,
@@ -42,14 +42,6 @@ module Metadata
 
       def title_display_data
         cocina_record.title_display_data.reject { it.label == 'Title' }
-      end
-
-      def publication_places
-        CocinaDisplay::DisplayData.from_strings(@cocina_record.publication_places, label: 'Place').presence
-      end
-
-      def publisher
-        CocinaDisplay::DisplayData.from_strings(@cocina_record.publisher_names, label: 'Publisher').presence
       end
     end
   end
